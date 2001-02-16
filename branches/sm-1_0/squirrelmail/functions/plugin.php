f5812aaee47ba9120247ad15afa3a3b70b0bcf5f
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-02-16 22:05:20 +0000

    This commit was manufactured by cvs2svn to create branch 'sm-1_0'.

---
+++ branches/sm-1_0/squirrelmail/functions/plugin.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ ** plugin.php
+ **
+ ** This file provides the framework for a plugin architecture.
+ **
+ ** Plugins will eventually be a way to provide added functionality
+ ** without having to patch the SquirrelMail source code. Have some
+ ** patience, though, as the these funtions might change in the near
+ ** future.
+ **
+ ** Documentation on how to write plugins might show up some time.
+ **
+ ** $Id$
+ **/
+
+
+   $plugin_php = true;
+   $plugin_general_debug = false;
+   
+   $squirrelmail_plugin_hooks = array();
+
+   // This function adds a plugin
+   function use_plugin ($name) {
+      global $plugin_general_debug;
+      
+      if (file_exists('../plugins/'.$name.'/setup.php')) {
+         if ($plugin_general_debug)
+	    echo "plugin:  --  Loading $name/setup.php<br>\n";
+         include ('../plugins/'.$name.'/setup.php');
+         $function = 'squirrelmail_plugin_init_'.$name;
+         if (function_exists($function))
+	 {
+	    if ($plugin_general_debug)
+	       echo "plugin:  ---- Executing $function to init plugin<br>\n";
+            $function($plugin_general_debug);
+	 }
+	 elseif ($plugin_general_debug)
+	    echo "plugin:  -- Init function $function doesn't exist.<br>\n";
+      }
+      elseif ($plugin_general_debug)
+         echo "plugin:  Couldn't find $name/setup.php<br>\n";
+   }
+
+   // This function executes a hook
+   function do_hook ($name) {
+      global $squirrelmail_plugin_hooks;
+      $Data = func_get_args();
+      if (isset($squirrelmail_plugin_hooks[$name]) && 
+          is_array($squirrelmail_plugin_hooks[$name])) {
+         foreach ($squirrelmail_plugin_hooks[$name] as $id => $function) {
+            // Add something to set correct gettext domain for plugin
+            if (function_exists($function)) {
+                $function($Data);
+            }
+         }
+      }
+      
+      // Variable-length argument lists have a slight problem when
+      // passing values by reference.  Pity.  This is a workaround.
+      return $Data;
+  }
+
+   // On startup, register all plugins configured for use
+   if (isset($plugins) && is_array($plugins))
+      foreach ($plugins as $id => $name)
+      {
+         if ($plugin_general_debug)
+	    echo "plugin:  Attempting load of plugin $name<br>\n";
+         use_plugin($name);
+      }
+
+   if ($plugin_general_debug)
+   {
+      echo "plugin:  Hook list<br>\n";
+      foreach ($squirrelmail_plugin_hooks as $Hook => $Plugins)
+      {
+          foreach ($Plugins as $Name => $Func)
+	  {
+	      echo "[$Hook][$Name] = $Func<br>\n";
+	  }
+      }
+   }
+
+?>
---
+++ branches/sm-1_0/squirrelmail/functions/prefs.php
@@ -0,0 +1,153 @@
+<?php
+   /**
+    **  prefs.php
+    **
+    **  This contains functions for manipulating user preferences
+    **
+    **  $Id$
+    **/
+
+   $prefs_php = true;
+
+   /** returns the value for $string **/
+   function getPref($data_dir, $username, $string) {
+      $filename = "$data_dir$username.pref";
+      if (!file_exists($filename)) {
+	 printf (_("Preference file %s not found. Exiting abnormally"), $filename);
+         exit;
+      }
+
+      $file = fopen($filename, "r");
+
+      /** read in all the preferences **/
+      for ($i=0; !feof($file); $i++) {
+         $pref = fgets($file, 1024);
+         if (substr($pref, 0, strpos($pref, "=")) == $string) {
+            fclose($file);
+            return trim(substr($pref, strpos($pref, "=")+1));
+         }
+      }
+      fclose($file);
+      return "";
+   }
+
+   function removePref($data_dir, $username, $string) {
+      $filename = "$data_dir$username.pref";
+      $found = false;
+      if (!file_exists($filename)) {
+	 printf (_("Preference file, %s, does not exist. Log out, and log back in to create a default preference file."), $filename);
+	 echo "<br>\n";
+         exit;
+      }
+      $file = fopen($filename, "r");
+
+      for ($i=0; !feof($file); $i++) {
+         $pref[$i] = fgets($file, 1024);
+         if (substr($pref[$i], 0, strpos($pref[$i], "=")) == $string) {
+            $i--;
+         }
+      }
+      fclose($file);
+
+      for ($i=0,$j=0; $i < count($pref); $i++) {
+         if (substr($pref[$i], 0, 9) == "highlight") {
+            $hlt[$j] = substr($pref[$i], strpos($pref[$i], "=")+1);
+            $j++;
+         }
+      }
+
+      $file = fopen($filename, "w");
+      for ($i=0; $i < count($pref); $i++) {
+         if (substr($pref[$i], 0, 9) != "highlight") {
+            fwrite($file, "$pref[$i]", 1024);
+         }   
+      }
+      if (isset($hlt)) {
+         for ($i=0; $i < count($hlt); $i++) {
+            fwrite($file, "highlight$i=$hlt[$i]");
+         }
+      }
+      fclose($file);
+   }
+   
+   /** sets the pref, $string, to $set_to **/
+   function setPref($data_dir, $username, $string, $set_to) {
+      $filename = "$data_dir$username.pref";
+      $found = false;
+      if (!file_exists($filename)) {
+	 printf (_("Preference file, %s, does not exist. Log out, and log back in to create a default preference file."), $filename);
+	 echo "\n<br>\n";
+         exit;
+      }
+      $file = fopen($filename, "r");
+
+      /** read in all the preferences **/
+      for ($i=0; !feof($file); $i++) {
+         $pref[$i] = fgets($file, 1024);
+         if (substr($pref[$i], 0, strpos($pref[$i], "=")) == $string) {
+            $found = true;
+            $pos = $i;
+         }
+      }
+      fclose($file);
+
+      $file = fopen($filename, "w");
+      if ($found == true) {
+         for ($i=0; $i < count($pref); $i++) {
+            if ($i == $pos) {
+               fwrite($file, "$string=$set_to\n", 1024);
+            } else {
+               fwrite($file, "$pref[$i]", 1024);
+            }
+         }
+      } else {
+         for ($i=0; $i < count($pref); $i++) {
+            fwrite($file, "$pref[$i]", 1024);
+         }
+         fwrite($file, "$string=$set_to\n", 1024);
+      }
+
+      fclose($file);
+   }
+
+
+
+
+   /** This checks if there is a pref file, if there isn't, it will
+       create it. **/
+   function checkForPrefs($data_dir, $username) {
+      $filename = "$data_dir$username.pref";
+      if (!file_exists($filename)) {
+         if (!copy("$data_dir" . "default_pref", $filename)) {
+            echo _("Error opening ") ."$filename";
+            exit;
+         }
+      }
+   }
+
+
+
+   /** Writes the Signature **/
+   function setSig($data_dir, $username, $string) {
+      $filename = "$data_dir$username.sig";
+      $file = fopen($filename, "w");
+      fwrite($file, $string);
+      fclose($file);
+   }
+
+
+
+   /** Gets the signature **/
+   function getSig($data_dir, $username) {
+      $filename = "$data_dir$username.sig";
+      $sig = "";
+      if (file_exists($filename)) {
+         $file = fopen($filename, "r");
+         while (!feof($file)) {
+            $sig .= fgets($file, 1024);
+         }
+         fclose($file);
+      }
+      return $sig;
+   }
+?>
