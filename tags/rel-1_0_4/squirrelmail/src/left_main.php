9492a131f1cecc18478c0b75c467689ad8438f5b
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-04-11 13:17:53 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1_0_4'.

---
+++ tags/rel-1_0_4/squirrelmail/src/left_main.php
@@ -0,0 +1,213 @@
+<?php
+   /**
+    **  left_main.php
+    **  Copyright (c) 1999-2000 The SquirrelMail development team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  This is the code for the left bar.  The left bar shows the folders
+    **  available, and has cookie information.
+    **
+    **  $Id$
+    **/
+
+   session_start();
+
+   if (!isset($i18n_php))
+      include ("../functions/i18n.php");
+
+   if(!isset($username)) {
+      set_up_language($squirrelmail_language, true);
+          include ("../themes/default_theme.php");
+          printf('<html><BODY TEXT="%s" BGCOLOR="%s" LINK="%s" VLINK="%s" ALINK="%s">',
+                          $color[8], $color[4], $color[7], $color[7], $color[7]);
+          echo "</body></html>";
+      exit;
+   }
+
+
+   if (!isset($strings_php))
+      include("../functions/strings.php");
+   if (!isset($config_php))
+      include("../config/config.php");
+   if (!isset($array_php))
+      include("../functions/array.php");
+   if (!isset($imap_php))
+      include("../functions/imap.php");
+   if (!isset($page_header_php))
+      include("../functions/page_header.php");
+   if (!isset($i18n_php))
+      include("../functions/i18n.php");
+   if (!isset($plugin_php))
+      include("../functions/plugin.php");
+
+   // open a connection on the imap port (143)
+   $imapConnection = sqimap_login($username, $key, $imapServerAddress, $imapPort, 10); // the 10 is to hide the output
+
+   /** If it was a successful login, lets load their preferences **/
+   include("../src/load_prefs.php");
+
+   displayHtmlHeader();
+
+   function formatMailboxName($imapConnection, $box_array, $delimeter) {
+      global $folder_prefix, $trash_folder, $sent_folder;
+      global $color, $move_to_sent, $move_to_trash;
+      global $unseen_notify, $unseen_type, $collapse_folders;
+
+      $real_box = $box_array['unformatted'];
+      $mailbox = $box_array['formatted'];
+      $mailboxURL = urlencode($real_box);
+      
+      $unseen = 0;
+      
+      if (($unseen_notify == 2 && $real_box == "INBOX") ||
+          $unseen_notify == 3) {
+         $unseen = sqimap_unseen_messages($imapConnection, $numUnseen, $real_box);
+         if ($unseen_type == 1 && $unseen > 0) {
+            $unseen_string = "($unseen)";
+            $unseen_found = true;
+         } else if ($unseen_type == 2) {
+            $numMessages = sqimap_get_num_messages($imapConnection, $real_box);
+            $unseen_string = "<font color=\"$color[11]\">($unseen/$numMessages)</font>";
+            $unseen_found = true;
+         }
+      }
+      
+      $special_color = false;
+      if ((strtolower($real_box) == "inbox") ||
+          (($real_box == $trash_folder) && ($move_to_trash)) ||
+          (($real_box == $sent_folder) && ($move_to_sent)))
+         $special_color = true;
+         
+      $spaces = '';
+      $line = "<NOBR>";
+      if (ereg("^( *)([^ ]*)$", $mailbox, $regs)) {
+          $spaces = $regs[1];
+          $mailbox = $regs[2];
+      }
+      
+      if ($unseen > 0)
+          $line .= "<B>";
+      $line .= str_replace(' ', '&nbsp;', $spaces);
+      
+      if ($collapse_folders) {
+         if (isset($box_array['parent']))
+            $line .= FoldLink($box_array['unformatted'], $box_array['parent']);
+         else
+            $line .= '<tt>&nbsp;</tt>&nbsp;';
+      }
+          
+      $line .= "<a href=\"right_main.php?sort=0&startMessage=1&mailbox=$mailboxURL\" target=\"right\" style=\"text-decoration:none\">";
+      if ($special_color == true)
+         $line .= "<FONT COLOR=\"$color[11]\">";
+      $line .= str_replace(' ', '&nbsp;', $mailbox);
+      if ($special_color == true)
+         $line .= "</font>";
+      $line .= "</a>";
+
+      if ($unseen > 0)
+         $line .= "</B>";
+
+      if (isset($unseen_found) && $unseen_found) {
+         $line .= "&nbsp;<small>$unseen_string</small>";
+      }
+
+      if (($move_to_trash == true) && ($real_box == $trash_folder)) {
+         if (! isset($numMessages))
+            $numMessages = sqimap_get_num_messages($imapConnection, $real_box);
+
+         if ($numMessages > 0)
+	 {
+            $urlMailbox = urlencode($real_box);
+            $line .= "\n<small>\n";
+            $line .= " &nbsp; (<B><A HREF=\"empty_trash.php\" style=\"text-decoration:none\">"._("purge")."</A></B>)";
+            $line .= "\n</small>\n";
+	 }
+      }
+      $line .= "</NOBR>";
+      return $line;
+   }
+
+   if (isset($left_refresh) && ($left_refresh != "None") && ($left_refresh != "")) {
+      echo "<META HTTP-EQUIV=\"Expires\" CONTENT=\"Thu, 01 Dec 1994 16:00:00 GMT\">\n";
+      echo "<META HTTP-EQUIV=\"Pragma\" CONTENT=\"no-cache\">\n"; 
+      echo "<META HTTP-EQUIV=\"REFRESH\" CONTENT=\"$left_refresh;URL=left_main.php\">\n";
+   }
+   
+   echo "\n<BODY BGCOLOR=\"$color[3]\" TEXT=\"$color[6]\" LINK=\"$color[6]\" VLINK=\"$color[6]\" ALINK=\"$color[6]\">\n";
+
+   do_hook("left_main_before");
+
+   $boxes = sqimap_mailbox_list($imapConnection);
+
+   echo "<CENTER><FONT SIZE=4><B>";
+   echo _("Folders") . "</B><BR></FONT>\n\n";
+
+   echo "<small>(<A HREF=\"../src/left_main.php\" TARGET=\"left\">";
+   echo _("refresh folder list");
+   echo "</A>)</small></CENTER><BR>";
+   $delimeter = sqimap_get_delimiter($imapConnection);
+
+   if (isset($collapse_folders) && $collapse_folders) {
+      if (isset($fold))
+         setPref($data_dir, $username, 'collapse_folder_' . $fold, 1);
+      if (isset($unfold))
+         setPref($data_dir, $username, 'collapse_folder_' . $unfold, 0);
+      $IAmAParent = array();
+      for ($i = 0; $i < count($boxes); $i ++) {
+          $parts = explode($delimeter, $boxes[$i]['unformatted']);
+          $box_name = array_pop($parts);
+          $box_parent = implode($delimeter, $parts);
+          $hidden = 0;
+          if (isset($box_parent)) {
+              $hidden = getPref($data_dir, $username, 
+                  'collapse_folder_' . $box_parent);
+              $IAmAParent[$box_parent] = $hidden;
+          }
+          $boxes[$i]['folded'] = $hidden;
+      }
+   }
+
+   for ($i = 0;$i < count($boxes); $i++) {
+      if (! isset($boxes[$i]['folded']) || ! $boxes[$i]['folded'])
+      {
+         $line = "";
+         $mailbox = $boxes[$i]["formatted"];
+
+         if (isset($collapse_folders) && $collapse_folders && isset($IAmAParent[$boxes[$i]['unformatted']])) {
+            $boxes[$i]['parent'] = $IAmAParent[$boxes[$i]['unformatted']];
+         }
+
+         if (in_array('noselect', $boxes[$i]['flags'])) {
+            $line .= "<FONT COLOR=\"$color[10]\">";
+            if (ereg("^( *)([^ ]*)$", $mailbox, $regs)) {
+                $line .= str_replace(' ', '&nbsp;', $regs[1]);
+                if (isset($boxes[$i]['parent']))
+                    $line .= FoldLink($boxes[$i]['unformatted'], 
+                        $boxes[$i]['parent']);
+                elseif ($collapse_folders)
+                    $line .= '<tt>&nbsp;</tt>&nbsp;';
+                $line .= str_replace(' ', '&nbsp;', $regs[2]);
+            }
+            $line .= '</FONT>';
+         } else {
+            $line .= formatMailboxName($imapConnection, $boxes[$i], $delimeter);
+         }
+         echo "$line<BR>\n";
+      }
+   }
+   sqimap_logout($imapConnection);
+   do_hook("left_main_after");
+
+   function FoldLink($mailbox, $folded) {
+       $mailbox = urlencode($mailbox);
+       echo '<tt><a target="left" style="text-decoration:none" ';
+       echo 'href="left_main.php?';
+       if ($folded)
+           echo "unfold=$mailbox\">+";
+       else
+           echo "fold=$mailbox\">-";
+       echo '</a></tt>&nbsp;';
+   }
+   
+?>
+</BODY></HTML>
---
+++ tags/rel-1_0_4/squirrelmail/src/options.php
@@ -0,0 +1,188 @@
+<?php
+   /**
+    **  options.php
+    **
+    **  Copyright (c) 1999-2000 The SquirrelMail development team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  Displays the options page. Pulls from proper user preference files
+    **  and config.php. Displays preferences as selected and other options.
+    **
+    **  $Id$
+    **/
+
+   session_start();
+
+   if (!isset($strings_php))
+      include('../functions/strings.php');
+   if (!isset($config_php))
+      include('../config/config.php');
+   if (!isset($page_header_php))
+      include('../functions/page_header.php');
+   if (!isset($display_messages_php))
+      include('../functions/display_messages.php');
+   if (!isset($imap_php))
+      include('../functions/imap.php');
+   if (!isset($array_php))
+      include('../functions/array.php');
+   if (!isset($i18n_php))
+      include('../functions/i18n.php');
+   if (!isset($auth_php))
+      include ('../functions/auth.php'); 
+
+   if (isset($language)) {
+      setcookie('squirrelmail_language', $language, time()+2592000);
+      $squirrelmail_language = $language;
+   }   
+
+   include('../src/load_prefs.php');
+   displayPageHeader($color, 'None');
+   is_logged_in(); 
+?>
+
+<br>
+<table width="95%" align="center" cellpadding="2" cellspacing="2" border="0">
+<tr><td bgcolor="<?php echo $color[0] ?>">
+   <center><b><?php echo _("Options") ?></b></center>
+</td></tr></table>
+
+<?php
+   if (isset($submit_personal)) {
+      # Save personal information
+      if (isset($full_name)) setPref($data_dir, $username, 'full_name', $full_name);
+      if (isset($email_address)) setPref($data_dir, $username, 'email_address', $email_address);
+      if (isset($reply_to)) setPref($data_dir, $username, 'reply_to', $reply_to);
+      if (isset($usesignature)) setPref($data_dir, $username, 'use_signature', $usesignature);  
+      if (isset($prefixsig)) setPref($data_dir, $username, 'prefix_sig', $prefixsig);
+      if (isset($signature_edit)) setSig($data_dir, $username, $signature_edit);
+      
+      do_hook('options_personal_save');
+      
+      echo '<br><center><b>'._("Successfully saved personal information!").'</b></center><br>';
+   } else if (isset($submit_display)) {  
+      # Save display preferences
+      setPref($data_dir, $username, 'chosen_theme', $chosentheme);
+      setPref($data_dir, $username, 'show_num', $shownum);
+      setPref($data_dir, $username, 'wrap_at', $wrapat);
+      setPref($data_dir, $username, 'editor_size', $editorsize);
+      setPref($data_dir, $username, 'left_refresh', $leftrefresh);
+      setPref($data_dir, $username, 'language', $language);
+      setPref($data_dir, $username, 'location_of_bar', $folder_new_location);
+      setPref($data_dir, $username, 'location_of_buttons', $button_new_location);
+      setPref($data_dir, $username, 'left_size', $leftsize);
+      setPref($data_dir, $username, 'use_javascript_addr_book', $javascript_abook);
+      if (isset($showhtmldefault))
+         setPref($data_dir, $username, 'show_html_default', 1);
+      else
+         removePref($data_dir, $username, 'show_html_default');
+    
+      do_hook('options_display_save');
+
+      echo '<br><center><b>'._("Successfully saved display preferences!").'</b><br>';
+      echo '<a href="../src/webmail.php" target=_top>' . _("Refresh Page") . '</a></center><br>';
+   } else if (isset($submit_folder)) { 
+      # Save folder preferences
+      if ($trash != 'none') {
+         setPref($data_dir, $username, 'move_to_trash', true);
+         setPref($data_dir, $username, 'trash_folder', $trash);
+      } else {
+         setPref($data_dir, $username, 'move_to_trash', '0');
+         setPref($data_dir, $username, 'trash_folder', 'none');
+      }
+      if ($sent != 'none') {
+         setPref($data_dir, $username, 'move_to_sent', true);
+         setPref($data_dir, $username, 'sent_folder', $sent);
+      } else {
+         setPref($data_dir, $username, 'move_to_sent', '0');
+         setPref($data_dir, $username, 'sent_folder', 'none');
+      } 
+      if (isset($folderprefix))
+         setPref($data_dir, $username, 'folder_prefix', $folderprefix);
+      setPref($data_dir, $username, 'unseen_notify', $unseennotify);
+      setPref($data_dir, $username, 'unseen_type', $unseentype);
+      if (isset($collapsefolders))
+          setPref($data_dir, $username, 'collapse_folders', $collapsefolders);
+      else
+          removePref($data_dir, $username, 'collapse_folders');
+      do_hook('options_folders_save');
+      echo '<br><center><b>'._("Successfully saved folder preferences!").'</b><br>';
+      echo '<a href="../src/left_main.php" target=left>' . _("Refresh Folder List") . '</a></center><br>';
+   } else {
+      do_hook('options_save');
+   }
+   
+?>
+
+
+<table width="90%" cellpadding="0" cellspacing="10" border="0" align="center">
+<tr>
+   <td width="50%" valign="top">
+      <table width="100%" cellpadding="3" cellspacing="0" border="0">
+         <tr>
+            <td bgcolor="<?php echo $color[9] ?>">
+               <a href="options_personal.php"><?php echo _("Personal Information"); ?></a>
+            </td>
+         </tr>
+         <tr>
+            <td bgcolor="<?php echo $color[0] ?>">
+               <?php echo _("This contains personal information about yourself such as your name, your email address, etc.") ?>
+            </td>
+         </tr>   
+      </table><br>
+      <table width="100%" cellpadding="3" cellspacing="0" border="0">
+         <tr>
+            <td bgcolor="<?php echo $color[9] ?>">
+               <a href="options_highlight.php"><?php echo _("Message Highlighting"); ?></a>
+            </td>
+         </tr>
+         <tr>
+            <td bgcolor="<?php echo $color[0] ?>">
+               <?php echo _("Based upon given criteria, incoming messages can have different background colors in the message list.  This helps to easily distinguish who the messages are from, especially for mailing lists.") ?>
+            </td>
+         </tr>   
+      </table><br>
+      <table width="100%" cellpadding="3" cellspacing="0" border="0">
+         <tr>
+            <td bgcolor="<?php echo $color[9] ?>">
+               <a href="options_order.php"><?php echo _("Index Order"); ?></a>
+            </td>
+         </tr>
+         <tr>
+            <td bgcolor="<?php echo $color[0] ?>">
+               <?php echo _("The order of the message index can be rearanged and changed to contain the headers in any order you want.") ?>
+            </td>
+         </tr>   
+      </table><br>
+   </td>
+   <td valign="top" width="50%">
+      <table width="100%" cellpadding="3" cellspacing="0" border="0">
+         <tr>
+            <td bgcolor="<?php echo $color[9] ?>">
+               <a href="options_display.php"><?php echo _("Display Preferences"); ?></a>
+            </td>
+         </tr>
+         <tr>
+            <td bgcolor="<?php echo $color[0] ?>">
+               <?php echo _("You can change the way that SquirrelMail looks and displays information to you, such as the colors, the language, and other settings.") ?>
+            </td>
+         </tr>   
+      </table><br>
+      <table width="100%" cellpadding="3" cellspacing="0" border="0">
+         <tr>
+            <td bgcolor="<?php echo $color[9] ?>">
+               <a href="options_folder.php"><?php echo _("Folder Preferences"); ?></a>
+            </td>
+         </tr>
+         <tr>
+            <td bgcolor="<?php echo $color[0] ?>">
+               <?php echo _("These settings change the way your folders are displayed and manipulated.") ?>
+            </td>
+         </tr>   
+      </table><br>
+   </td>
+</tr>
+</table>
+   <?php
+      do_hook('options_link_and_description')
+   ?>
+</body></html>
