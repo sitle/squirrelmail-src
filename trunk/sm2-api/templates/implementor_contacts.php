8ae705bf3ffe40c599070e9fd88c8e32d152160c
thomppj <thomppj@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-07-17 05:46:13 +0000

    More template files in place...

---
+++ trunk/sm2-api/templates/implementor_contacts.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ *  Squirrelmail2 API
+ *  Copyright (c) 2001 Th Squirrelmail Foundation
+ *  Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ *  $Id$
+ **/
+
+?>
