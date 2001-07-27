5f03300f4396483f618479db92fcdf1dec9e2a0c
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-07-27 21:17:42 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1_1_3'.

---
+++ tags/rel-1_1_3/squirrelmail/src/options_display.php
@@ -0,0 +1,268 @@
+<?php
+   /**
+    **  options_display.php
+    **
+    **  Copyright (c) 1999-2000 The SquirrelMail development team
+    **  Licensed under the GNU GPL. For full terms see the file COPYING.
+    **
+    **  Displays all optinos about display preferences
+    **
+    **  $Id$
+    **/
+
+   include('../src/validate.php');
+   include('../functions/display_messages.php');
+   include('../functions/imap.php');
+   include('../functions/array.php');
+   include('../functions/plugin.php');
+   
+   displayPageHeader($color, 'None');
+   $chosen_language = getPref($data_dir, $username, 'language');  
+?>
+   <br>
+<table width="95%" align="center" border="0" cellpadding="2" cellspacing="0">
+<tr><td bgcolor="<?php echo $color[0] ?>" align="center">
+
+      <b><?php echo _("Options") . ' - ' . _("Display Preferences"); ?></b><br>
+
+    <table width="100%" border="0" cellpadding="1" cellspacing="1">
+    <tr><td bgcolor="<?php echo $color[4] ?>" align="center">
+
+   <form name="f" action="options.php" method="post"><br>
+      <table width="100%" cellpadding="2" cellspacing="0" border="0">
+         <tr>
+            <td align="right" nowrap><?php echo _("Theme"); ?>:
+            </td><td>
+<?php
+   echo '         <tt><select name="chosentheme">' . "\n";
+   for ($i = 0; $i < count($theme); $i++) {
+      if ($theme[$i]['PATH'] == $chosen_theme)
+         echo '         <option selected value="'.$theme[$i]['PATH'].'">'.$theme[$i]['NAME']."\n";
+      else
+         echo '         <option value="'.$theme[$i]['PATH'].'">'.$theme[$i]['NAME']."\n";
+   }
+   echo '         </select></tt>';  
+?>
+            </td>
+         </tr>
+         <tr>
+            <td align="right" nowrap><?php echo _("Language"); ?>:
+            </td><td>
+<?php
+   echo '         <tt><select name="language">' . "\n";
+   foreach ($languages as $code => $name) {
+      if (! isset($name['ALIAS'])) {
+         if ($code==$chosen_language)
+            echo '         <OPTION SELECTED VALUE="'.$code.'">'.$name['NAME']."\n";
+         else
+            echo '         <OPTION VALUE="'.$code.'">'.$name['NAME']."\n";
+      }
+   }
+   echo '         </select></tt>';  
+   if (! $use_gettext)
+      echo '<br><small>This system doesn\'t support multiple languages</small>';
+      
+?>
+            </td>
+         <tr>
+            <td align=right nowrap>&nbsp;
+               <?php echo _("Use Javascript or HTML addressbook?") . '</td><td>'; 
+               if ($use_javascript_addr_book == true) {
+                  echo '         <input type="radio" name="javascript_abook" value="1" checked> ' . _("JavaScript") . '&nbsp;&nbsp;&nbsp;&nbsp;';
+                  echo '         <input type="radio" name="javascript_abook" value="0"> ' . _("HTML"); 
+               } else {
+                  echo '         <input type="radio" name="javascript_abook" value="1"> ' . _("JavaScript") . '&nbsp;&nbsp;&nbsp;&nbsp;';
+                  echo '         <input type="radio" name="javascript_abook" value="0" checked> ' . _("HTML"); 
+               }  
+               ?>
+            </td>
+         </tr>
+         <tr>
+            <td align=right nowrap><?php echo _("Number of Messages to Index"); ?>:
+            </td><td>
+<?php
+   if (isset($show_num))
+      echo '         <tt><input type="text" size="5" name="shownum" value="'.$show_num.'"></tt><br>';
+   else
+      echo '         <tt><input type="text" size="5" name="shownum" value="25"></tt><br>'; 
+?>
+            </td>
+         </tr>
+         <tr>
+            <td align="right" nowrap><?php echo _("Wrap incoming text at"); ?>:
+            </td><td>
+<?php
+   if (isset($wrap_at))
+      echo '         <tt><input type="text" size="5" name="wrapat" value="'.$wrap_at.'"></tt><br>';
+   else
+      echo '         <tt><input type="tex" size="5" name="wrapat" value="86"></tt><br>'; 
+?>
+            </td>
+         </tr>
+         <tr>
+            <td align="right" nowrap><?php echo _("Size of editor window"); ?>:
+            </td><td>
+<?php
+   if ($editor_size >= 10 && $editor_size <= 255)
+      echo '         <tt><input type="text" size="5" name="editorsize" value="'.$editor_size.'"></tt><br>';
+   else
+      echo '         <tt><input type="text" size="5" name="editorsize" value="76"></tt><br>'; 
+?>
+            </td>
+         </tr>
+         <tr>
+            <td align="right" nowrap><?PHP echo _("Location of folder list") ?>:</td>
+            <td><select name="folder_new_location">
+                <option value="left"<?PHP
+                    if ($location_of_bar != 'right') echo ' SELECTED';
+                    ?>><?PHP echo _("Left"); ?></option>
+                <option value="right"<?PHP
+                    if ($location_of_bar == 'right') echo ' SELECTED';
+                    ?>><?PHP echo _("Right"); ?></option>
+                </select>
+            </td>
+         </tr>
+         <tr>
+            <td align="right" nowrap><?PHP echo _("Location of buttons when composing") ?>:</td>
+            <td><select name="button_new_location">
+                <option value="top"<?PHP
+                    if ($location_of_buttons == 'top') echo ' SELECTED';
+                    ?>><?PHP echo _("Before headers"); ?></option>
+                <option value="between"<?PHP
+                    if ($location_of_buttons == 'between') echo ' SELECTED';
+                    ?>><?PHP echo _("Between headers and message body"); ?></option>
+                <option value="bottom"<?PHP
+                    if ($location_of_buttons == 'bottom') echo ' SELECTED';
+                    ?>><?PHP echo _("After message body"); ?></option>
+                </select>
+            </td>
+         </tr>
+         <tr>
+            <td align=right nowrap><?php echo _("Width of folder list"); ?>:
+            </td><td>
+<?php
+   echo '         <select name="leftsize">' . "\n";
+   for ($i = 100; $i <= 300; $i += 10)
+   {
+       if ($left_size >= $i && $left_size < $i + 10)
+          echo "<option value=\"$i\" selected>$i pixels\n";
+       else
+          echo "<option value=\"$i\">$i pixels\n";
+   }
+   echo '         </select>';  
+?>
+            </td>
+         </tr>
+         <tr>
+            <td align="right" nowrap><?php echo _("Auto refresh folder list"); ?>:
+            </td><td>
+<?php
+   $seconds_str = _("Seconds");
+   $none_str = _("None");
+   $minute_str = _("Minute");
+   $minutes_str = _("Minutes");
+
+   echo '               <SELECT name="leftrefresh">';
+   
+   if ($left_refresh == '')
+      $left_refresh = 'None';
+   if ($left_refresh > 600)
+      $left_refresh = 600;
+   RefreshOption($left_refresh, '', 'None', _("None"));
+   RefreshOption($left_refresh, 30);
+   RefreshOption($left_refresh, 60);
+   RefreshOption($left_refresh, 120);
+   RefreshOption($left_refresh, 180);
+   RefreshOption($left_refresh, 300);
+   RefreshOption($left_refresh, 600);
+   // Refreshes after the session auto-timeout (default 15 min) is pointless
+
+function RefreshOption(&$current, $val, $str = '') {
+   static $lastVal = 0;
+   
+   if (is_int($val) && is_int($current)) {
+      if ($current > $lastVal && $current <= $val)
+         $current = $val;
+   }
+   
+   if ($str == '') {
+      if ($val > 60) {
+         $str = ($val / 60) . ' ' . _("Minutes");
+      } elseif ($val == 60) {
+         $str = '1 ' . _("Minute");
+      } else {
+         $str = $val . ' ' . _("Seconds");
+      }
+   }
+   
+   echo '<option value="' . $val . '"';
+   if ($val == $current)
+      echo ' SELECTED';
+   echo '>' . $str . "\n";
+}
+      echo '               </SELECT>'; 
+?>
+            </td>
+         </tr>
+         <tr>
+            <td align="right">
+                <?php echo _("Use alternating row colors?") ?>
+            </td><td>
+<?php
+    if (isset($alt_index_colors) && $alt_index_colors == 1) {
+        $a = " checked";
+        $b = "";
+    } else {
+        $a = "";
+        $b = " checked";
+    }
+?>
+                <input type="radio" name="altIndexColors" value="1"<?php echo $a ?>> <?php echo _("Yes") ?> &nbsp;&nbsp; 
+                <input type="radio" name="altIndexColors" value="0"<?php echo $b ?>> <?php echo _("No") ?><br>
+            </td>
+         </tr>
+         <tr>
+            <td align=right>
+               <?php echo _("Show HTML version by default"); ?>:
+            </td>
+            <td>
+               <input type=checkbox name=showhtmldefault <?php 
+	       if (isset($show_html_default) && $show_html_default) 
+	       echo " checked"; ?>>
+	         <?php 
+echo _("Yes, show me the HTML version of a mail message, if it is available."); 
+                 ?>
+            </td>
+         </tr>
+         <tr>
+            <td align=right>
+               <?php echo _("Include Self"); ?>:
+            </td>
+            <td>
+               <input type=checkbox name=includeselfreplyall <?php 
+	       if (getPref($data_dir, $username, 'include_self_reply_all')
+	           != '')
+	       echo " checked"; ?>>
+	         <?php 
+echo _("Don't remove me from the CC addresses when I use \"Reply All\"");
+                 ?>
+            </td>
+         </tr>
+         <?php do_hook('options_display_inside'); ?>
+         <tr>
+            <td>&nbsp;
+            </td><td>
+               <input type="submit" value="<?php echo _("Submit"); ?>"name="submit_display">
+            </td>
+         </tr>
+      </table>
+   </form>
+
+   <?php do_hook('options_display_bottom'); ?>
+
+    </td></tr>
+    </table>
+
+</td></tr>
+</table>
+</body></html>
---
+++ tags/rel-1_1_3/squirrelmail/src/options_identities.php
@@ -0,0 +1,337 @@
+<?php
+   /**
+    **  options_identities.php
+    **
+    **  $Id$
+    **/
+
+   include('../src/validate.php');
+   include('../functions/display_messages.php');
+
+   if (isset($return)) {
+      SaveUpdateFunction();
+      header('Location: options_personal.php');
+      exit();
+   }
+   
+   displayPageHeader($color, 'None');
+
+   $Info = do_hook('options_identities_process', 0);
+   if ($Info[1])
+      SaveUpdateFunction();
+
+   if (CheckAndDoDefault() || CheckAndDoPromote()) {
+      SaveUpdateFunction();
+   }
+   if (isset($update) || CheckForDelete())
+      SaveUpdateFunction();
+
+   LoadInfo($full_name, $email_address, $reply_to, '');
+
+?>
+<br>
+<table width=95% align=center border=0 cellpadding=2 cellspacing=0>
+<tr><td bgcolor="<?php echo $color[0] ?>" align="center">
+
+      <b><?php echo _("Options") . ' - ' . _("Advanced Identities"); ?></b>
+
+    <table width="100%" border="0" cellpadding="1" cellspacing="1">
+    <tr><td bgcolor="<?php echo $color[4] ?>" align="center">
+
+<form name=f action="options_identities.php" method=post><br>
+
+<?PHP do_hook('options_identities_top'); ?>
+
+<table width=80% cellpadding=2 cellspacing=0 border=0>
+  <tr bgcolor="<?PHP echo $color[9] ?>">
+    <th colspan=2 align=center><?PHP echo _("Default Identity") ?></th>
+  </tr>
+<?PHP
+
+   ShowTableInfo($full_name, $email_address, $reply_to, '');
+  
+   $num = 1;
+   while (LoadInfo($full_name, $email_address, $reply_to, $num))
+   {
+?>
+  <tr bgcolor="<?PHP echo $color[9] ?>">
+    <th colspan=2 align=center><?PHP printf (_("Alternate Identity %d"),
+    $num) ?></th>
+  </tr>
+<?PHP
+       ShowTableInfo($full_name, $email_address, $reply_to, $num);
+       $num ++;
+   }
+   
+?>
+  <tr bgcolor="<?PHP echo $color[9] ?>">
+    <th colspan=2 align=center><?PHP echo _("Add a New Identity") ?></th>
+  </tr>
+<?
+
+   ShowTableInfo('', '', '', $num);
+?>
+</table>   
+</form>
+
+    </td></tr>
+    </table>
+
+</td></tr>
+</table>
+</body></html>
+
+<?PHP
+
+function SaveUpdateFunction()
+{
+   global $username, $data_dir, $full_name, $email_address, $reply_to;
+    
+   $i = 1;
+   $fakeI = 1;
+   $name = 'form_for_' . $i;
+   global $$name;
+   while (isset($$name))
+   {
+      $name = 'delete_' . $i;
+      global $$name;
+      if (isset($$name)) {
+         $fakeI --;
+      } else {
+         do_hook('options_identities_renumber', $i, $fakeI);
+         $filled = 0;
+	 
+         $name = 'full_name' . $i;
+         global $$name;
+	 if ($$name != '') 
+	    $filled ++;
+         setPref($data_dir, $username, 'full_name' . $fakeI, $$name);
+	 
+         $name = 'email_address' . $i;
+         global $$name;
+	 if ($$name != '') 
+	    $filled ++;
+         setPref($data_dir, $username, 'email_address' . $fakeI, $$name);
+	 
+         $name = 'reply_to' . $i;
+         global $$name;
+	 if ($$name != '') 
+	    $filled ++;
+         setPref($data_dir, $username, 'reply_to' . $fakeI, $$name);
+	 
+	 if ($filled == 0) 
+	    $fakeI --;
+      }
+      
+      $fakeI ++;
+      $i ++;
+      $name = 'form_for_' . $i;
+      global $$name;
+   }
+   
+   setPref($data_dir, $username, 'identities', $fakeI);
+   
+   while ($fakeI != $i)
+   {
+      removePref($data_dir, $username, 'full_name' . $fakeI);
+      removePref($data_dir, $username, 'email_address' . $fakeI);
+      removePref($data_dir, $username, 'reply_to' . $fakeI);
+      $fakeI ++;
+   }
+
+   setPref($data_dir, $username, 'full_name', $full_name);
+   setPref($data_dir, $username, 'email_address', $email_address);
+   setPref($data_dir, $username, 'reply_to', $reply_to);
+}
+
+function CheckAndDoDefault()
+{
+   global $username, $data_dir, $full_name, $email_address, $reply_to;
+    
+   $i = 1;
+   $name = 'form_for_' . $i;
+   global $$name;
+   while (isset($$name))
+   {
+      $name = 'make_default_' . $i;
+      global $$name;
+      if (isset($$name)) {
+          do_hook('options_identities_renumber', $i, 'default');
+          global $full_name, $email_address, $reply_to;
+	  
+          $name = 'full_name' . $i;
+          global $$name;
+          $temp = $full_name;
+          $full_name = $$name;
+          $$name = $temp;
+          
+          $name = 'email_address' . $i;
+          global $$name;
+          $temp = $email_address;
+          $email_address = $$name;
+          $$name = $temp;
+          
+          $name = 'reply_to' . $i;
+          global $$name;
+          $temp = $reply_to;
+          $reply_to = $$name;
+          $$name = $temp;
+          
+          return true;
+      }
+      
+      $i ++;
+      $name = 'form_for_' . $i;
+      global $$name;
+   }
+   return false;
+}
+
+function CheckForDelete()
+{
+   global $username, $data_dir, $full_name, $email_address, $reply_to;
+    
+   $i = 1;
+   $name = 'form_for_' . $i;
+   global $$name;
+   while (isset($$name))
+   {
+      $name = 'delete_' . $i;
+      global $$name;
+      if (isset($$name)) {
+          return true;
+      }
+      
+      $i ++;
+      $name = 'form_for_' . $i;
+      global $$name;
+   }
+   return false;
+}
+
+function CheckAndDoPromote()
+{
+   global $username, $data_dir, $full_name, $email_address, $reply_to;
+    
+   $i = 1;
+   $name = 'form_for_' . $i;
+   global $$name;
+   while (isset($$name))
+   {
+      $name = 'promote_' . $i;
+      global $$name;
+      if (isset($$name) && $i > 1) {
+          do_hook('options_identities_renumber', $i, $i - 1);
+	  
+	  $nameA = 'full_name' . $i;
+	  $nameB = 'full_name' . ($i - 1);
+	  global $$nameA, $$nameB;
+	  $temp = $$nameA;
+	  $$nameA = $$nameB;
+	  $$nameB = $temp;
+	  
+	  $nameA = 'email_address' . $i;
+	  $nameB = 'email_address' . ($i - 1);
+	  global $$nameA, $$nameB;
+	  $temp = $$nameA;
+	  $$nameA = $$nameB;
+	  $$nameB = $temp;
+	  
+	  $nameA = 'reply_to' . $i;
+	  $nameB = 'reply_to' . ($i - 1);
+	  global $$nameA, $$nameB;
+	  $temp = $$nameA;
+	  $$nameA = $$nameB;
+	  $$nameB = $temp;
+	  
+	  return true;
+      }
+
+      $i ++;
+      $name = 'form_for_' . $i;
+      global $$name;
+   }
+   return false;
+}
+
+function LoadInfo(&$n, &$e, &$r, $post)
+{
+   global $username, $data_dir;
+
+   $n = getPref($data_dir, $username, 'full_name' . $post);
+   $e = getPref($data_dir, $username, 'email_address' . $post);
+   $r = getPref($data_dir, $username, 'reply_to' . $post);
+
+   if ($n != '' || $e != '' || $r != '')
+      return true;
+}
+
+function ShowTableInfo($full_name, $email_address, $reply_to, $post)
+{
+   global $color;
+   
+   $OtherBG = ' bgcolor="' . $color[0] . '"';
+   if ($full_name == '' && $email_address == '' && $reply_to == '')
+   	  $OtherBG = '';
+
+   if ($full_name == '' && $email_address == '' && $reply_to == '')
+      $isEmptySection = true;
+   else
+      $isEmptySection = false;
+
+?>
+  <tr<?PHP echo $OtherBG ?>>
+    <td align=right nowrap>
+      <?php echo _("Full Name"); ?>:
+    </td>
+    <td>
+      <input size=50 type=text value="<?php echo htmlspecialchars($full_name)
+      ?>" name="full_name<?PHP echo $post ?>"> 
+    </td>
+  </tr>
+  <tr<?PHP echo $OtherBG ?>>
+    <td align=right nowrap>
+      <?php echo _("E-Mail Address"); ?>:
+    </td>
+    <td>
+      <input size=50 type=text value="<?php echo htmlspecialchars($email_address)
+      ?>" name="email_address<?PHP echo $post ?>"> 
+    </td>
+  </tr>
+  <tr<?PHP echo $OtherBG ?>>
+    <td align=right nowrap>
+      <?php echo _("Reply To"); ?>:
+    </td>
+    <td>
+      <input size=50 type=text value="<?php echo htmlspecialchars($reply_to)
+      ?>" name="reply_to<?PHP echo $post ?>"> 
+    </td>
+  </tr>
+<?PHP do_hook('options_identities_table', $OtherBG, $isEmptySection, $post); ?>
+  <tr<?PHP echo $OtherBG ?>>
+    <td>&nbsp;</td><td>
+      <input type=hidden name="form_for_<?PHP echo $post ?>" value="1">
+      <input type=submit name="update" value="<?PHP echo _("Save / Update") ?>">
+<?PHP 
+   if (! $isEmptySection && $post != '') {
+?>
+      <input type=submit name="make_default_<?PHP echo $post ?>" value="<?PHP
+         echo _("Make Default") ?>">
+      <input type=submit name="delete_<?PHP echo $post ?>" value="<?PHP echo _("Delete") ?>">
+<?PHP
+   }
+   if (! $isEmptySection && $post != '' && $post > 1) {
+?>
+      <input type=submit name="promote_<?PHP echo $post ?>" value="<?PHP
+         echo _("Move Up") ?>">
+<?PHP
+   }
+   do_hook('options_identities_buttons', $isEmptySection, $post);
+?>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2">&nbsp;</td>
+  </tr>
+<?PHP
+}
