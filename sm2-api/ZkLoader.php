f2ceeca1e719ffd90ea7cc2a6d05083ae7436880
thomppj <thomppj@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-09-07 02:40:48 +0000

    Made some major changes to make the terminology more consistent. I was using the terms provider, implementor, and module in directory/file names, documentation, method and function names, and variable names...all to mean the same thing. So I decided to standardize on module (and variations thereof).
    Also, did extensive work on the ZkLoader class, the lib/auth/load.php
    script, and so forth. Just started work on lib/logging/services.php, too.
    The core of the work in this commit is the code required to automagically
    load the zookeeper core and modules without having to hard code all their
    names (etc) into ZkLoader, etc.
    
    Seriously need to document the way ZkLoader works and the convention for
    naming, creating, etc services (lib/*) and their corresponding modules.
    
    I have decided that the Zookeeper core and modules are going to use the
    Zookeeper error handling service to report errors. This is because the
    functionality provided by the error handling service is so core and central
    to the needs of the rest of the Zookeeper code. This is similar to the fact
    that some of the Zookeeper core and modules will require session handling
    to be enabled (such as the auth service).
    
    Lastly, created the file ZkFunctions.php that will contain miscellaneous
    functions used by the zookeeper core. This file is NOT NOT NOT a place
    to put code specific in much detail to any of the services or modules,
    as that code (if needing to be shared) should probably be incorporated
    into the service itself. Again, this file is only for reusable functions
    specific to the Zookeeper core. It will probably remain relatively small.

--- sm2-api/ZkLoader.php
+++ sm2-api/ZkLoader.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
- * Squirrelmail2 API
- * Copyright (c) 2001 Th Squirrelmail Foundation
+ * Zookeeper
+ * Copyright (c) 2001 Paul Joseph Thompson
  * Licensed under the GNU GPL. For full terms see the file COPYING.
  *
  * $Id$
@@ -11,12 +11,12 @@
 /**
  * ZkLoader
  *
- * The ZkLoader class allows the resources provides by Zookeeper to
- * be loaded by an external application.
+ * The ZkLoader class allows the resources provides by Zookeeper to be
+ * loaded by an external application.
  */
 class ZkLoader {
     var $appname;
-    var $zookeeper_home;
+    var $zkhome;
     var $libhome;
     var $modhome;
 
@@ -26,26 +26,75 @@ class ZkLoader {
      * @param string $appname name of the calling application
      * @param string $zookeeper_home home directory for zookeeper
      */
-    function implementor_auth($appname, $zookeeper_home) {
+    function ZkLoader($appname, $zkhome) {
         $this->appname = $appname;
-        $this->zookeeper_home = $zookeeper_home;
-        $this->libhome = "$zookeeper_home/lib";
-        $this->modhome = "$zookeeper_home/modules";
+        $this->zkhome = $zkhome;
+        $this->libhome = "$zkhome/lib";
+        $this->modhome = "$zkhome/modules";
+        
+        require_once("$zkhome/ZkFunctions.php");
     }
 
     /**
-     * Check a 
+     * Attempt to load the requested service.
      *
      * @param string $username username with which to authenticate
      * @param string $password password with which to authenticate
      * @return bool indicates correct or incorrect password
      */
-    function loadService($service, $provider, $options) {
-        switch ($service) {
-            case 'auth':
-                require_once("$this->libhome/auth/load.php");
-                require_once("$this->modhome/auth/ZkAuthImp_$provider.php");
+    function loadService($svcname, $options, $modname = '') {
+        $svcfile  = "$this->libhome/$svcname/load.php";
+        $svcfunc  = "zkload_$svcname";
+        $svcclass = "ZkSvc_$svcname";
+        $modfile  = "$this->modhome/$svcname/$modname.php";
+        $modclass = "ZkMod_$svcname" . "_$modname";
+        
+        /* Do some checks on the service name, then load the service file. */
+        if (!zkCheckName($svcname)) {
+            return (false);
+        } else if (!file_exists($svcfile)) {
+            return (false);
+        } else {
+            require_once($svcfile);
+            $code_preload = "\$svcfile_result = $svcfunc('$this->zkhome');";
+            
+            /* Run the preload code string. */
+            eval($code_preload);
+            
+            /* Evaluate the result. */
+            if (!$svcfile_result) {
+                return (false);
+            }
         }
+        
+        /* Begin creation of the service load code string. */
+        $code_loadservice = "\$service = new $svcclass(\$options";
+        
+        /* Check if we need to load a module for this service. */
+        if ($modname != '') {
+            /* Do some checks on the module name, then load the module file. */
+            if (!zkCheckName($modname)) {
+                return (false);
+            } else if (!file_exists($modfile)) {
+                return (false);
+            } else {
+                require_once($modfile);
+            }
+
+            /* Continue building the service and module load code strings. */
+            $code_loadmodule = "\$module = new $modclass(\$options);";
+            $code_loadservice .= ', $module';
+        }
+        
+        /* Finish up the service load code string. */
+        $code_loadservice .= ');';
+        
+        /* Run the service and module load code strings. */
+        eval($code_loadmodule);
+        eval($code_loadservice);
+        
+        /* Return the newly created Zookeeper service. */
+        return ($service);
     }
 }
 
