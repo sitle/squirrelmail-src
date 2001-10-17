19471431845216f21aa5e736cd88f68a94a00629
thomppj <thomppj@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2001-10-17 04:19:34 +0000

    Moved lib to services.

---
+++ trunk/sm2-api/services/session/load.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *  Zookeeper
+ *  Copyright (c) 2001 Paul Joseph Thompson
+ *  Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ *  $Id$
+ **/
+ 
+function zkload_session($zookeeper_home) {
+    /* Require a session to have already been started. */
+    if (session_id() == '') {
+        return (false);
+    }
+
+    /* Load the zookeeper session classes. */
+    require_once("$zookeeper_home/lib/session/service.php");
+    return (true);
+}
+
+?>
