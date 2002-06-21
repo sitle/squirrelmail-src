e17dbeb873a1af19d2b798c024bc40f28c41d77b
graf25 <graf25@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-06-21 02:00:00 +0000

    Small bugfix -- we already called htmlentities when encoding the string. Only htmlspecialchars needed at this point.

--- squirrelmail/src/printer_friendly_bottom.php
+++ squirrelmail/src/printer_friendly_bottom.php
@@ -92,13 +92,13 @@ displayHtmlHeader( _("Printer Friendly"), '', FALSE );
 echo "<body text=\"$color[8]\" bgcolor=\"$color[4]\" link=\"$color[7]\" vlink=\"$color[7]\" alink=\"$color[7]\">\n" .
      /* headers (we use table because translations are not all the same width) */
      '<table>'.
-     '<tr><td>' . _("From") . ':</td><td>' . htmlentities($from) . "</td></tr>\n".
-     '<tr><td>' . _("To") . ':</td><td>' . htmlentities($to) . "</td></tr>\n";
+     '<tr><td>' . _("From") . ':</td><td>' . htmlspecialchars($from) . "</td></tr>\n".
+     '<tr><td>' . _("To") . ':</td><td>' . htmlspecialchars($to) . "</td></tr>\n";
 if ( strlen($cc) > 0 ) { /* only show CC: if it's there... */
-     echo '<tr><td>' . _("CC") . ':</td><td>' . htmlentities($cc) . "</td></tr>\n";
+     echo '<tr><td>' . _("CC") . ':</td><td>' . htmlspecialchars($cc) . "</td></tr>\n";
 }
-echo '<tr><td>' . _("Date") . ':</td><td>' . htmlentities($date) . "</td></tr>\n".
-     '<tr><td>' . _("Subject") . ':</td><td>' . htmlentities($subject) . "</td></tr>\n".
+echo '<tr><td>' . _("Date") . ':</td><td>' . htmlspecialchars($date) . "</td></tr>\n".
+     '<tr><td>' . _("Subject") . ':</td><td>' . htmlspecialchars($subject) . "</td></tr>\n".
      '</table>'.
      "\n";
 /* body */
