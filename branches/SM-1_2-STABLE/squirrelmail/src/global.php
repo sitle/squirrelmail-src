ea487012766b259fca9f049f131f65b993cfe3fc
jmunro <jmunro@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-08-01 15:37:27 +0000

    I thought my helper function was clever, but it does not scope right :(

--- branches/SM-1_2-STABLE/squirrelmail/src/global.php
+++ branches/SM-1_2-STABLE/squirrelmail/src/global.php
@@ -9,6 +9,7 @@
  * This includes code to update < 4.1.0 globals to the newer format 
  * It also has some (only one so far) handy-dandy global variable functions
  *
+ * $Id$
  */
 
 
@@ -30,92 +31,4 @@ if ( (float)substr(PHP_VERSION,0,3) < 4.1 ) {
   $_SERVER  =& $HTTP_SERVER_VARS;
   $_SESSION =& $HTTP_SESSION_VARS;
 }
-
-
-/* function to make superglobals act just like register_globals
- * = On for the duration of the script. it maps a global group's
- * key => value pairs to variable_name => value like so:
- *
- * $_GET['mailbox'] => "INBOX" 
- *
- *  changes to:
- *
- * $mailbox = "INBOX";
- *
- * default is SQUIRREL. other options are:
- *
- * GET          Variables supplied by an HTTP GET
- * POST         Variables supplied by an HTTP POST
- * SESSION      Variables registered in the session
- * COOKIE       Variables supplied by an HTTP cookie
- * ENV          Variables supplied by the environment
- * SERVER       Variables set by the web server
- * FILES        Variables set by HTTP POST file uploads
- * SQUIRREL     POST, GET, COOKIE, and SESSION vars
- * ALL          The whole GLOBALS array     
- *
- */
-
-
-function extract_globals( $group ) {
-    switch ($group) {
-        case 'GET':
-            if (!empty($_GET)) {
-                extract($_GET);
-            }
-            break;
-        case 'POST':
-            if (!empty($_POST)) {
-                extract($_POST);
-            }
-            break;
-        case 'SESSION':
-            if (!empty($_SESSION)) {
-                extract($_SESSION);
-            }
-            break;
-        case 'COOKIE':
-            if (!empty($_COOKIE)) {
-                extract($_COOKIE);
-            }
-            break;
-        case 'ENV':
-            if (!empty($_ENV)) {
-                extract($_ENV);
-            }
-            break;
-        case 'SERVER':
-            if (!empty($_SERVER)) {
-                extract($_SERVER);
-            }
-            break;
-        case 'FILES':
-            if (!empty($_FILES)) {
-                extract($_FILES);
-            }
-            break;
-        case 'ALL':
-            if (!empty($GLOBALS)) {
-                extract($GLOBALS);
-            }
-            break;
-        case 'SQUIRREL':
-        default:
-            if (!empty($_GET)) {
-                extract($_GET);
-            }
-            if (!empty($_POST)) {
-                extract($_POST);
-            }
-            if (!empty($_SESSION)) {
-                extract($_SESSION);
-            }
-            if (!empty($_COOKIE)) {
-                extract($_COOKIE);
-            }
-            break;
-    }
-}               
-
-
-            
+?>
