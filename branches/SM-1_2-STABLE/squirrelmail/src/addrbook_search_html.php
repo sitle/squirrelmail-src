d4cf653c3fbae2249c2a7b88281b35fb77a9a91f
jmunro <jmunro@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-08-16 07:27:01 +0000

    more rg=0 fixes. All the $HTTP_ style globals should now be converted to the newer $_ style. attachments should work now also. Tested with PHP 4.0.6 and 4.1.2.

--- branches/SM-1_2-STABLE/squirrelmail/src/addrbook_search_html.php
+++ branches/SM-1_2-STABLE/squirrelmail/src/addrbook_search_html.php
@@ -25,6 +25,9 @@ require_once('../functions/addressbook.php');
 require_once('../functions/plugin.php');
 require_once('../functions/strings.php');
 
+
+$session = $_POST['session'];
+$mailbox = $_POST['mailbox'];
 if ( isset($_POST['addrquery']) ) {
     $addrquery = $_POST['addrquery'];
 }
@@ -34,6 +37,7 @@ if ( isset($_POST['listall']) ) {
 if ( isset($_POST['backend'] ) ) {
     $backend = $_POST['backend'];
 }
+
 
 /* Insert hidden data */
 function addr_insert_hidden() {
@@ -93,6 +97,9 @@ if ($javascript_on) {
             '&nbsp;&nbsp;'.
             '<a href="#" onClick="CheckAll(\'B\');">' . _("All") . '</a>';
     }
+    else {
+        $chk_all = '';
+    }
     echo '<TABLE BORDER="0" WIDTH="98%" ALIGN="center">' .
            '<TR BGCOLOR="' . $color[9] . '"><TH ALIGN="left">&nbsp;' . $chk_all . '</TH>' .
           '<TH ALIGN="left">&nbsp;' . _("Name") . '</TH>' .
@@ -137,7 +144,6 @@ if ($javascript_on) {
 
 /* --- End functions --- */
 
-global $mailbox;
 if ($compose_new_win == '1') {
     compose_Header($color, $mailbox);
 }
