a80364d0833f4029ac120b3cc2c4f860fe8ac1ae
tassium <tassium@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-10-23 19:45:20 +0000

    Added cram_md5_response($challenge) - calculates & returns cram-md5 reply Added digest_md5_parse_challenge($challenge) - extracts challenge details

--- branches/tassium-auth/squirrelmail/functions/auth.php
+++ branches/tassium-auth/squirrelmail/functions/auth.php
@@ -37,4 +37,51 @@ function is_logged_in() {
     }
 }
 
+function cram_md5_response ($username,$password,$challenge) {
+
+/* Given the challenge from the server, supply the response using
+   cram-md5 (See RFC 2195 for details)
+   NOTE: Requires mhash extension to PHP
+*/
+$challenge=base64_decode($challenge);
+$hash=bin2hex(mhash(MHASH_MD5,$challenge,$password));
+$response=base64_encode($username . " " . $hash) . "\r\n";
+return $response;
+}
+
+function digest_md5_parse_challenge($challenge) {
+/* This function parses the challenge sent during DIGEST-MD5 authentication and
+   returns an array. See the RFC for details on what's in the challenge string.
+*/
+  $challenge=base64_decode($challenge);
+  while (strlen($challenge)) {
+    if ($challenge{0} == ',') { // First char is a comma, must not be 1st time through loop
+      $challenge=substr($challenge,1);
+    }
+    $key=explode('=',$challenge,2);
+    $challenge=$key[1];
+    $key=$key[0];
+    if ($challenge{0} == '"') {
+      // We're in a quoted value
+      // Drop the first quote, since we don't care about it
+      $challenge=substr($challenge,1);
+      // Now explode() to the next quote, which is the end of our value
+      $val=explode('"',$challenge,2);
+      $challenge=$val[1]; // The rest of the challenge, work on it in next iteration of loop
+      $value=explode(',',$val[0]);
+      // Now, for those quoted values that are only 1 piece..
+      if (sizeof($value) == 1) {
+        $value=$value[0];  // Convert to non-array
+      }
+    } else {
+      // We're in a "simple" value - explode to next comma
+      $val=explode(',',$challenge,2);
+      $challenge=$val[1];
+      $value=$val[0];
+    }
+    $parsed["$key"]=$value;
+  } // End of while loop
+  return $parsed;
+}
+
 ?>
