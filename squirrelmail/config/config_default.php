5822334d8399a68d86c4aaf7b1322a489f50dd9b
tassium <tassium@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-10-29 15:03:24 +0000

    I had incremented the config file version number.  This commit backs out that change, since the format didn't really change, I only added an option or two.  conf.pl sets sane defaults if the loaded config.php doesn't already have those options set, so I don't see a reason to ask the user to reconfig from scratch.

--- squirrelmail/config/config_default.php
+++ squirrelmail/config/config_default.php
@@ -11,7 +11,7 @@
 
 /* Do not change this value. */
 global $config_version;
-$config_version = '1.2.1';
+$config_version = '1.2.0';
 
 /* Organization's logo picture (blank if none) */
 global $org_logo;
