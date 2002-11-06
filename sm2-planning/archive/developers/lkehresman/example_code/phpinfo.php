131adfc6f5aee6105516d12536a2e1da476f7f04
sizzlingmercury <sizzlingmercury@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-11-06 03:27:03 +0000

    add to archive;

---
+++ sm2-planning/archive/developers/lkehresman/example_code/phpinfo.php
@@ -0,0 +1,245 @@
+<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN"
+"http://www.w3.org/TR/REC-html40/loose.dtd">
+<html><head>
+<!-- ViewCVS -- http://viewcvs.sourceforge.net/
+by Greg Stein -- mailto:gstein@lyra.org
+-->
+<title>CVS log for squirrelmail/sm2-planning/developers/lkehresman/example_code/Attic/phpinfo.php</title>
+</head>
+<body text="#000000" bgcolor="#ffffff">
+<center>
+<nolayer>
+<iframe SRC="http://sfads.osdn.com/1.html" width="468" height="60" frameborder="no" border="0" MARGINWIDTH="0" MARGINHEIGHT="0" SCROLLING="no"><a HREF="http://sfads.osdn.com/cgi-bin/ad_default.pl?click"><img SRC="http://sfads.osdn.com/cgi-bin/ad_default.pl?display" border=0 height="60" width="468"></a>
+</iframe>
+</nolayer>
+</center>
+<table width="100%" border=0 cellspacing=0 cellpadding=0>
+<tr>
+<td><h1>CVS log for squirrelmail/sm2-planning/developers/lkehresman/example_code/Attic/phpinfo.php</h1></td>
+<td align=right><img src="/sourceforge_whitebg.gif" alt="(logo)" border=0
+width=136 height=79></td>
+</tr>
+</table>
+<a href="/cgi-bin/viewcvs.cgi/squirrelmail/sm2-planning/developers/lkehresman/example_code/?only_with_tag=MAIN&hideattic=0#phpinfo.php"><img src="/icons/back.gif" alt="(back)" border=0
+width=16 height=16></a>
+<b>Up to <a href="http://sourceforge.net/">[SourceForge]</a> / <a href="/cgi-bin/viewcvs.cgi/squirrelmail/?only_with_tag=MAIN&hideattic=0#dirlist">squirrelmail</a> / <a href="/cgi-bin/viewcvs.cgi/squirrelmail/sm2-planning/?only_with_tag=MAIN&hideattic=0#dirlist">sm2-planning</a> / <a href="/cgi-bin/viewcvs.cgi/squirrelmail/sm2-planning/developers/?only_with_tag=MAIN&hideattic=0#dirlist">developers</a> / <a href="/cgi-bin/viewcvs.cgi/squirrelmail/sm2-planning/developers/lkehresman/?only_with_tag=MAIN&hideattic=0#dirlist">lkehresman</a> / <a href="/cgi-bin/viewcvs.cgi/squirrelmail/sm2-planning/developers/lkehresman/example_code/?only_with_tag=MAIN&hideattic=0#dirlist">example_code</a></b><p>
+<a href="#diff">Request diff between arbitrary revisions</a>
+
+<hr noshade>
+
+Default branch: MAIN
+<br>
+Bookmark a link to:
+
+<a href="phpinfo.php?rev=HEAD&only_with_tag=MAIN&hideattic=0&content-type=text/vnd.viewcvs-markup"><b>HEAD</b></a>
+/
+(<a href="/cgi-bin/viewcvs.cgi/*checkout*/squirrelmail/sm2-planning/developers/lkehresman/example_code/Attic/phpinfo.php?rev=HEAD&only_with_tag=MAIN&hideattic=0&content-type=text/plain" target="cvs_checkout"
+onClick="window.open('about:blank', 'cvs_checkout',
+'resizeable=1,scrollbars=1')"
+><b>download</b></a>)
+
+
+<br>
+
+Current tag: MAIN <br>
+
+
+<hr size=1 noshade>
+
+Revision <b>1.3</b>
+
+
+, <i>Wed Nov  6 02:48:05 2002 UTC</i> (37 minutes, 41 seconds ago) by <i>sizzlingmercury</i>
+
+<br>Branch:
+
+<a href="phpinfo.php?only_with_tag=MAIN&hideattic=0"><b>MAIN</b></a>
+
+
+
+<br>CVS Tags:
+
+<a href="phpinfo.php?only_with_tag=HEAD&hideattic=0"><b>HEAD</b></a>
+
+
+
+
+
+<br>Changes since <b>1.2: +0 -0 lines</b>
+
+
+
+<br><b><i>FILE REMOVED</i></b>
+
+<pre>move stuff to archive
+</pre>
+
+<hr size=1 noshade>
+
+<a name="rev1.2"></a>
+
+
+Revision
+
+<a href="/cgi-bin/viewcvs.cgi/*checkout*/squirrelmail/sm2-planning/developers/lkehresman/example_code/Attic/phpinfo.php?rev=1.2&only_with_tag=MAIN&hideattic=0" target="cvs_checkout"
+onClick="window.open('about:blank', 'cvs_checkout',
+'resizeable=1,scrollbars=1')"
+><b>1.2</b></a>
+
+
+
+
+/
+<a href="phpinfo.php?rev=1.2&only_with_tag=MAIN&hideattic=0&content-type=text/vnd.viewcvs-markup"><b>(view)</b></a>
+
+
+- <a href="phpinfo.php?annotate=1.2&only_with_tag=MAIN&hideattic=0">annotate</a>
+
+
+- <a href="phpinfo.php?r1=1.2&only_with_tag=MAIN&hideattic=0">[select for diffs]</a>
+
+
+
+, <i>Fri Jan  5 23:28:08 2001 UTC</i> (21 months, 4 weeks ago) by <i>lkehresman</i>
+
+<br>Branch:
+
+<a href="phpinfo.php?only_with_tag=MAIN&hideattic=0"><b>MAIN</b></a>
+
+
+
+
+
+
+<br>Changes since <b>1.1: +1 -1 lines</b>
+
+
+
+
+<br>Diff to <a href="phpinfo.php.diff?r1=1.1&r2=1.2&only_with_tag=MAIN&hideattic=0">previous 1.1</a>
+
+
+
+
+
+
+
+<pre>fixed syntax error
+</pre>
+
+<hr size=1 noshade>
+
+<a name="rev1.1"></a>
+
+
+Revision
+
+<a href="/cgi-bin/viewcvs.cgi/*checkout*/squirrelmail/sm2-planning/developers/lkehresman/example_code/Attic/phpinfo.php?rev=1.1&only_with_tag=MAIN&hideattic=0" target="cvs_checkout"
+onClick="window.open('about:blank', 'cvs_checkout',
+'resizeable=1,scrollbars=1')"
+><b>1.1</b></a>
+
+
+
+
+/
+<a href="phpinfo.php?rev=1.1&only_with_tag=MAIN&hideattic=0&content-type=text/vnd.viewcvs-markup"><b>(view)</b></a>
+
+
+- <a href="phpinfo.php?annotate=1.1&only_with_tag=MAIN&hideattic=0">annotate</a>
+
+
+- <a href="phpinfo.php?r1=1.1&only_with_tag=MAIN&hideattic=0">[select for diffs]</a>
+
+
+
+, <i>Fri Jan  5 23:25:34 2001 UTC</i> (22 months ago) by <i>lkehresman</i>
+
+<br>Branch:
+
+<a href="phpinfo.php?only_with_tag=MAIN&hideattic=0"><b>MAIN</b></a>
+
+
+
+
+
+
+
+
+
+
+
+<pre>added for testing
+</pre>
+
+<a name=diff></a>
+<hr noshade>
+This form allows you to request diffs between any two revisions of
+a file. You may select a symbolic revision name using the selection
+box or you may type in a numeric name using the type-in text box.
+<p>
+<form method="GET" action="phpinfo.php.diff?only_with_tag=MAIN&hideattic=0" name="diff_select">
+Diffs between
+<select name="r1">
+<option value="text" selected>Use Text Field</option>
+
+<option value="0.1:MAIN">MAIN</option>
+
+<option value="1.3:HEAD">HEAD</option>
+
+</select>
+<input type="TEXT" size="12" name="tr1" value="1.1"
+onChange="document.diff_select.r1.selectedIndex=0">
+and
+<select name="r2">
+<option value="text" selected>Use Text Field</option>
+
+<option value="0.1:MAIN">MAIN</option>
+
+<option value="1.3:HEAD">HEAD</option>
+
+</select>
+<input type="TEXT" size="12" name="tr2" value="1.3"
+onChange="document.diff_select.r1.selectedIndex=0">
+<br>Type of Diff should be a
+<select name="diff_format" onchange="submit()">
+<option value="h" selected>Colored Diff</option>
+<option value="l" >Long Colored Diff</option>
+<option value="u" >Unidiff</option>
+<option value="c" >Context Diff</option>
+<option value="s" >Side by Side</option>
+</select>
+<input type=submit value=" Get Diffs "></form>
+<hr noshade>
+
+<a name=branch></a>
+<form method="GET" action="phpinfo.php">
+<input type=hidden name="hideattic" value="0">
+View only Branch:
+<select name="only_with_tag" onchange="submit()">
+<option value="" >Show all branches</option>
+
+<option value="MAIN" selected>MAIN</option>
+
+</select>
+<input type=submit value=" View Branch ">
+</form>
+
+<a name=logsort></a>
+<form method="GET" action="phpinfo.php">
+<input type=hidden name="hideattic" value="0">
+Sort log by:
+<select name="logsort" onchange="submit()">
+<option value="cvs" >Not sorted</option>
+<option value="date" selected>Commit date</option>
+<option value="rev" >Revision</option>
+</select>
+<input type=submit value=" Sort ">
+</form>
+<hr noshade>
+<table width="100%" border=0 cellpadding=0 cellspacing=0><tr>
+<td align=left><address><a href="http://sourceforge.net/">Back to SourceForge</a></address></td>
+<td align=right>
+Powered by<br><a href="http://viewcvs.sourceforge.net/">ViewCVS 0.8</a>
+</td></tr></table>
+</body></html>
