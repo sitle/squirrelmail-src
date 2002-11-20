6c6406153f4742820da902747ef7612bda49c852
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-11-20 04:26:05 +0000

    New parts for planning docs

---
+++ trunk/sm2-planning/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * index.php - Index for planning documents
+ *
+ * Copyright (c) 1999-2002 The SquirrelMail development team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ *
+ * $Id$
+ */
+include_once('common_header.inc');
+
+print_header();
+
+?>
+
+</body>
+</html>
