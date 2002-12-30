22134bccc0069bf2e41ef05f4b46ff213457cf1d
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2002-12-30 20:43:20 +0000

    Updated to reflect what was implemented in CVS

--- trunk/sm2-planning/config_revisions.php
+++ trunk/sm2-planning/config_revisions.php
@@ -32,30 +32,42 @@ paths to elements potentially located outside of the SM tree.
 <P><a href="plugin_revisions.php">Plugin Revisions</a> are also 
 relevant to this discussion.
 
-
-
 <?php print_links(); ?>
 <a name="versioning"></a>
 <H2>Versioning</H2>
+
+<p><b>Target Release: 1.3.x and up (added 12/29 to 1.3.3 CVS)</b></p>
+
 Due to changes in the 1.3.x stream (SM_PATH, etc.) and upcoming changes
 slated for 1.4/1.5, it's now necessary to allow plugins to check for the 
 SquirrelMail version, in order to procede properly.
 
 <p>To that end, FUTURE versions of SM (including the current dev stream), 
-will define an internal global constant, $sm_internal_version, as an array
-containing 3 parts (major, minor, release) (global.php).
+will define an internal global constant, $SQM_INTERNAL_VERSION, as an array
+containing 3 parts (release, major, minor) 
+(defined in functions/strings.php - with other version constant).
 
 <p>Also in global.php will be a version checking function that will take 
-arguments similar to those provided for php version checking, and will return true if the current version is >= the one specified (just as check_php_version does).
+arguments similar to those provided for php version checking, and will 
+return true if the current version is >= the one specified 
+(just as check_php_version does).
+
+<p class="ebullient">&lt;ebullient - 12/29&gt;<br />
+Updated this section to reflect what I added to 1.3.3 CVS (release/major/minor).
+The constants in strings.php (for version) should be moved to sm_init.php, 
+in my opinion (as described below).<br />
+&lt;/ebullient - 12/29&gt;</p>
 
 <?php print_links(); ?>
 <a name="config"></a>
 <H2>Configuration Process</H2>
+<p><b>Target Release: 1.5</b></p>
 
 <?php print_links(); ?>
 <a name="config"></a>
 <H2>Setting Paths/SM Initialization - sm_init.php</H2>
 
+<p><b>Target Release: 1.5</b></p>
 
 </body>
 </html>
