3c6232dc2e787f79ec352a6bc8d3875f1c432b03
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-03-05 04:29:02 +0000

    And on we go. Love stupid errors ..

--- smdoc/site-config.php
+++ smdoc/site-config.php
@@ -1,3 +1,4 @@
+<?php
 /*
  * Created by SquirrelMail Development Team
  * Place site/location specific config vars in this config file
@@ -13,3 +14,12 @@ define('DBNAME', 'wtf'); // database name
 define('DBUSER', 'root'); // database username
 define('DBPASS', ''); // database password
 
+/* debugging */
+define('DEBUG', FALSE); // show debug information
+define('DEBUG_VAR', TRUE); // show variable debug information
+define('DEBUG_TRACE', TRUE); // show trace debug information
+define('DEBUG_SQL', TRUE); // show SQL debug information
+define('DEBUG_TIME', TRUE); // show execution time
+define('RENDER', TRUE); // use rendering engine (FALSE bypasses rendering phase)
+
+?>
