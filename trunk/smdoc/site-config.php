3abc3d4194a9fbead1d9419168cdbfcd64c1fb26
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-03-05 04:14:11 +0000

    Sample site config, customize as necessary

---
+++ trunk/smdoc/site-config.php
@@ -0,0 +1,15 @@
+/*
+ * Created by SquirrelMail Development Team
+ * Place site/location specific config vars in this config file
+ * $Id$
+ */
+
+define('PATH','/path/to/wtfw/install');
+
+/* database settings */
+define('DATABASE', 'mysql'); // database type (MySQL is the only DB module at the moment)
+define('DBHOST', 'localhost'); // database IP address
+define('DBNAME', 'wtf'); // database name
+define('DBUSER', 'root'); // database username
+define('DBPASS', ''); // database password
+
