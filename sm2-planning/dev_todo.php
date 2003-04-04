6ced8421a3bae40da471d95ebad10f73d1b54b7e
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-04-04 04:58:06 +0000

    Minor updates.. getting ready for bigger brain dump

---
+++ sm2-planning/dev_todo.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * plugin_revisions.php - Discussion of changes to plugin model
+ *
+ * Copyright (c) 1999-2002 The SquirrelMail development team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+include_once('common_header.inc');
+set_title('Development Items');
+set_original_author('ebullient');
+set_attributes('$Author$','$Revision$','$Date$');
+print_header();
+?>
+
+<UL>
+<LI>UI Restructure
+    who: ebullient
+<LI>IMAP Message/Mailbox restructure
+    who: stekkel
+<LI>Plugin architecture revision (<a href="http://localhost/sm2/plugin_revisions.php">here</a>)<br />
+    who: 
+<LI>Config system revision ( <a href="http://localhost/sm2/config_revisions.php">here</a> )
+    who:
+<LI>Remove other languages/locales from main SM download (common plugin)
+    who:
+</UL>
+
+</body>
+</html>
