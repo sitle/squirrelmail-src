ca4f467e7d0d19bdcb2517bb16459a378b405ef8
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-04-04 23:16:28 +0000

    Major changes to the edit menu at the bottom of every page. Simplified, only shows things if they are doable, and the user has permission

--- trunk/smdoc/sqmail.wikimenu.php
+++ trunk/smdoc/sqmail.wikimenu.php
@@ -23,14 +23,21 @@ function sqmNavMenu(&$thing) {
         echo '<a href="', THINGIDURI, $wtf->user->objectid, '&amp;class=user&amp;op=edit">', $wtf->user->title, '</a> ';
 		echo '( <a href="', THINGIDURI, $wtf->user->objectid, '&amp;class=user&amp;op=logout">Logout</a> )';
 	}    
+// Current workspace for User
     if ($wtf->user->workspaceid != 0 ) {
        $workspace = &wtf::loadObject($wtf->user->workspaceid, 0, 'workspace');
        echo '<br/>Current Workspace: ';
        echo '<a href="', THINGIDURI, $workspace->objectid, '&amp;class=workspace">' . $workspace->title . '</a>';
     }
-    echo '</usermenu>';        	
-// Workspace/PageTitle
-    echo '<a href="'.THINGURI.$thing->title.'&amp;class='.$wtf->class.'">'.$thing->title.'</a>';
+    echo '</usermenu>';      
+  	
+// PageTitle/Workspace of page (not shown if Main)
+    if ( $thing->classid != HARDTHINGCLASSID ) {
+        echo '<a href="'.THINGURI.$thing->title.'&amp;class='.$wtf->class.'">'.$thing->title.'</a>';
+    } else {
+        // Hardthings require slightly different links
+        echo '<a href="'.THINGURI.$wtf->thingtitle.'">'.$thing->title.'</a>';
+    }
     if ( $thing->workspaceid != 0 ) {
       if ( $thing->workspaceid != $wtf->user->workspaceid ) {
         $workspace = &wtf::loadObject($wtf->user->workspaceid, 0, 'workspace');
@@ -56,33 +63,52 @@ function sqmNavMenu(&$thing) {
 
 /* display the wiki menu */
 function sqmEditMenu(&$thing) {
+    global $HARDTHING;
 	global $wtf;
 	track('wikiMenu');
 	
 	echo '<editmenu>';
-	
-// page
-    echo 'This Page: ';
-	echo '<a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=view">View</a> ';
-	echo '| <a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=edit">Edit</a> ';
-	echo '| <a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=delete">Delete</a> ';
-	echo '| <a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=history">History</a> ';
-	if ($wtf->user->inGroup(GODS)) {
-		echo '| <a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=admin">Administrate</a> ';
+    if ( $thing->classid != HARDTHINGCLASSID ) {
+        // None of these functions (including History) are available for Hard Things
+        if ( $wtf->user->inGroup($thing->viewGroup) ) {
+	        echo '<a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=view">View</a> | ';
+        }
+        if ( $wtf->user->inGroup($thing->editGroup) ) {
+	        echo '<a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=edit">Edit</a> | ';
+        }
+        if ( $wtf->user->inGroup($thing->deleteGroup) ) {
+  	       echo '<a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=delete">Delete</a> | ';
+        }
+        if ($wtf->user->inGroup($thing->adminGroup)) {
+            echo '<a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=admin">Administrate</a> | ';
+        }
+	    echo '<a href="', THINGIDURI, $wtf->thingid, '&amp;class=', $wtf->class, '&amp;version=', $wtf->thing->version, '&amp;op=history">History</a> | ';
+        echo '<a href="', THINGURI, 'recentchanges">Recent Changes</a>';
+        echo '<br/>';
+
+        // page updator information
+        echo 'This Page ';
+        $updatorDetails = $thing->getUpdator();
+	    if ($updatorDetails) {
+		    echo ' last edited by <a href="'. THINGIDURI, $updatorDetails['homeid'], '&amp;class=home">', $updatorDetails['username'], '</a> on '.dbdate2string($updatorDetails['datetime'], SHORTDATEFORMAT);
+	    } else {
+		    $creatorDetails = $thing->getCreator();
+		    echo ' created by <a href="', THINGIDURI.$creatorDetails['homeid'].'&amp;class=home">', $creatorDetails['username'], '</a> on ', dbdate2string($creatorDetails['datetime'], SHORTDATEFORMAT);
+        }
+    } else {
+        // Here is the much stripped down footer for Hard Things
+	    echo '<a href="', THINGURI, 'recentchanges">Recent Changes</a>';
+        echo '<br/>';
+        if ( isset($HARDTHING[$thing->objectid]['lastmodified']) ) {
+            preg_match("/Date: (\d+)\/(\d+)\/(\d+) (\d+):(\d+):(\d+) /", $HARDTHING[$thing->objectid]['lastmodified'], $matches);
+            $last_modified = mktime($matches[4],$matches[5],$matches[6],$matches[2],$matches[3],$matches[1]);
+        } else {
+            $last_modified = filemtime(__FILE__);
+        }
+        echo 'This Page last updated on ' . date( "F d Y H:i.", $last_modified);
 	}
-    $updatorDetails = $thing->getUpdator();
-	if ($updatorDetails) {
-		echo ' (last edited by <a href="'. THINGIDURI, $updatorDetails['homeid'], '&amp;class=home">', $updatorDetails['username'], '</a> on '.dbdate2string($updatorDetails['datetime'], SHORTDATEFORMAT), ') ';
-	} else {
-		$creatorDetails = $thing->getCreator();
-		echo ' (created by <a href="', THINGIDURI.$creatorDetails['homeid'].'&amp;class=home">', $creatorDetails['username'], '</a> on ', dbdate2string($creatorDetails['datetime'], SHORTDATEFORMAT), ') ';
-	}
-    echo '<br/>';
-// site
-    echo 'Site: ';
-	echo '<a href="', THINGURI, 'wikipage">Create a New Page</a> | ';
-	echo '<a href="', THINGURI, 'recent changes">View Recent Changes</a> ';
-	
+
+// close edit menu
 	echo '</editmenu>';
 	track();
 }
