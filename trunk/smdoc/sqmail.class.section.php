f634b781f897e542a7a2e6a0fbabb27e0963cea2
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-04-16 05:55:18 +0000

    First pass at sections. I'm sure I forgot something.

--- trunk/smdoc/sqmail.class.section.php
+++ trunk/smdoc/sqmail.class.section.php
@@ -31,7 +31,7 @@ if (!defined('SECTIONCREATE')) define('SECTIONCREATE', GODS);
      * As will anything that has not been added to a section yet.
      */
     
-class section extends content { 
+class section extends wikipage { 
 
     var $objectlist;
 
@@ -39,13 +39,14 @@ class section extends content {
 	function section(
         &$user,
         $title = NULL,
+        $content = '',
         $viewGroup = DEFAULTVIEWGROUP,
         $editGroup = DEFAULTEDITGROUP,
         $deleteGroup = DEFAULTDELETEGROUP,
         $adminGroup = DEFAULTADMINGROUP
 	) {
 		track('section::section', $title, $viewGroup, $editGroup, $deleteGroup, $adminGroup);
-		parent::thing($user, $title, '', FALSE, $viewGroup, $editGroup, $deleteGroup, $adminGroup);
+		parent::wikipage($user, $title, $content, $viewGroup, $editGroup, $deleteGroup, $adminGroup);
 		$this->objectlist = array();
 		track();
 	}
@@ -60,13 +61,13 @@ class section extends content {
             echo '<section_edit_failed '.
                  'message="Could not add thing to section, as thing already belongs to a section."/>';
             $success = false;
-        } else { 
+        } else {
+            $obj->sectionid = $this->objectid;
             array_push($this->objectlist, 
                        array('objectid' => $obj->objectid, 
                              'classname' => get_class($obj), 
                              'title' => $obj->title));
             $this->save();
-            $obj->sectionid = $this->objectid;
         }
         track();
         return $success;
@@ -130,75 +131,169 @@ class section extends content {
         return $sections;
     }  
 
-    function drawForm($url, $title = NULL, $titleIsEditable = FALSE, $loadtime = NULL ) {
+    function drawForm($url, $title = NULL, $titleIsEditable = FALSE, $content = NULL, 
+                      $comment = NULL, $small = NULL, $loadtime = NULL ) {
         global $wtf;
         if (isset($this)) {
             $objectid = $this->objectid;
             $version = $this->version;
             $updatorid = $this->updatorid;
+            $objectlist = $this->objectlist;
             if ($title == NULL) $title = $this->title;
+            if ($content == NULL) $content = $this->content;
         } else {
             $objectid = '';
             $version = 1;
             $updatorid = NULL;
+            $objectlist == NULL;
             if ($title == NULL) $title = '';
+            if ($content == NULL) $content = $this->content;
         }
         if ($loadtime == NULL) $loadtime = time();
 
-        echo '<section_form url="', $url, '" ';
+        echo '<content_form url="', $url, '" ';
         echo 'thingidfield="thingid" thingid="', $objectid, '" ';
         echo 'versionfield="version" version="', $version, '" ';
         echo 'loadtimefield="loadtime" loadtime="'.$loadtime.'" ';
-        echo 'submit="submit">';
+        echo 'submit="submit" preview="preview">';
+
         if ($titleIsEditable) {
-            echo '<section_titlebox name="title" maxlength="', MAXTITLELENGTH, '">', $title, '</section_titlebox>';
+            echo '<content_titlebox name="title" maxlength="', MAXTITLELENGTH, '">', $title, '</content_titlebox>';
         } else {
-            echo '<section_title title="', $title, '" version="', $version, '"/>';
+            echo '<content_title title="', $title, '" version="', $version, '"/>';
         }
 
-        echo '</section_form>';
+        # If this section owns something
+        echo '<sectionlist_edit>';
+
+        $num_objects = count($objectlist);
+        if ( $objectlist != NULL && $num_objects > 0 ) {
+            foreach ( $objectlist as $key => $obj ) {
+                $value  = 'url="' . $url . '" ';
+                $value .= 'key="' . $key . '"> ';
+                $value .= $obj['title'];
+                if ( $num_objects == 1 ) {
+                    echo '<sectionlist_edit_item ' . $value . '</sectionlist_edit_item>';
+                } elseif ( $key == 0 ) {
+                    echo '<sectionlist_edit_item_down ' . $value . '</sectionlist_edit_item_down>';
+                } elseif ( $key == $num_objects - 1 ) {
+                    echo '<sectionlist_edit_item_up ' . $value . '</sectionlist_edit_item_up>';
+                } else {
+                    echo '<sectionlist_edit_item_updown ' . $value . '</sectionlist_edit_item_updown>';
+                }
+            }
+        }
+        echo '</sectionlist_edit>';
+
+        echo '<wikipage_canvas name="content" maxlength="', MAXCONTENTLENGTH, '">', $content, '</wikipage_canvas>';
+
+        # If you're the same person as the last updator and not Anonymous, 
+        # give you the option of making a "small" update to cover those oopses.
+        if ($wtf->user->objectid == $updatorid && $wtf->user->objectid != ANONYMOUSUSERID && isset($small)) {
+            if ($small == 'on') {
+                echo '<content_smallupdate checked="checked" name="small"/>';
+            } else {
+                echo '<content_smallupdate name="small"/>';
+            }
+        }
+        if ($comment) {
+            echo '<wikipage_comment name="comment" maxlength="', MAXWIKIPAGECOMMENTLENGTH, '"></wikipage_comment>';
+        }
+        echo '</content_form>';
     }
+
+// get content
+    function getContent($newContent = NULL) {
+        global $wtf;
+        $content = parent::getContent($newContent);
+
+        // remove </wikipage> closing tag appended by parent
+        $content = substr($content, 0, 0 - strlen('</wikipage>'));
+    
+        // add our section list
+        $content .= '<sectionlist>';
+        if (isset($this)) {
+            foreach ($this->objectlist as $obj) {
+                $content .= '<sectionlist_item>';
+                $url = THINGIDURI.$obj['objectid'].'&amp;class='.$obj['classname'];
+                $content .= '<a href="'.$url.'">'.$obj['title'].'</a>';
+                $content .= '</sectionlist_item>';
+            }
+        }
+        $content .= '</sectionlist>';
+
+        // re-append the </wikipage> closing tag
+        $content .= '</wikipage>';
+        return $content;
+    }
+
+// delete
+	function delete() {
+        track('section::delete');
+        if ( count($this->objectlist) > 0 ) {
+            // loop through objects listed in the section, and remove them from the list
+            foreach ( $this->objectlist as $obj_elem ) {
+                $obj = wtf::loadObject($obj_elem['objectid'], 0, $obj_elem['classname']);
+                if ( $this->removeFromSection($obj) ) {
+                    $obj->save(); // save changes to object if successfully removed from session
+                } 
+            }
+        }
+        // delete thing
+        track();
+        return parent::delete();
+    }
+
 
 /*** Methods ****/
 
-// view
-    function method_view() {
-        global $wtf;
-        track('section::method::view');
-        if (getValue('version', FALSE)) {
-            echo '<thing_info version="'.$this->version.'" class="'.get_class($this).'"/>';
-        }
-        if (hasPermission($this, $wtf->user, 'viewGroup')) {
-            echo 'Section content here';
-        } else {
-            echo '<thing_permissionerror method="view" title="'.$this->title.'"/>';
-        }
-        track();
-    }
- 
 // create
-    function method_create($thingName = NULL, $objectName = 'section') { // this is both a method and a static member
+    function method_create($thingName = NULL, $objectName = 'section') { 
+    // this is both a method and a static member
         global $conn, $wtf;
         track('section::method::create');
         if ($wtf->user->inGroup(SECTIONCREATE)) {
             if (isset($this)) {
-                $url = THINGIDURI.$this->objectid.'&amp;class='.get_class($this).'&amp;op=create';
+                $url = THINGIDURI.$this->objectid.'&class='.get_class($this).'&op=create';
                 $objectName = get_class($this);
             } else {
                 $url = THINGURI.$thingName.'&amp;class=hardclass';
             }
 
             $create = getValue('submit', FALSE);
+            $preview = getValue('preview', FALSE);
+
             $title = getValue('title', FALSE);
-            if ($create) { // if the form has been submitted, create the object
+            $content = getValue('content', FALSE);
+
+            if ($create || $preview ) { 
                 $thing = new $objectName(
                     $wtf->user,
                     $title
                 );
                 if ($thing && $thing->objectid != 0) { // thing created
-                    $thing->save();
-                    header('Location: '.THINGIDURI.$thing->objectid.'&class='.get_class($thing));
-                    exit;
+                    if ( $create ) { // creat thing
+                        $result = $thing->update($content, FALSE, FALSE);
+                        if ( $result['success'] ) {
+                            $thing->save();
+                            header('Location: '.THINGIDURI.$thing->objectid.'&class='.get_class($thing));
+                            exit;
+                        } else {
+                            echo '<content_create_failed message="', $result['error'], '"/>';
+                            echo '<syntax>', $result['syntaxCheck'], '</syntax>';
+                        }
+                    } elseif ( $preview ) {
+                        $result = $thing->preview($content);
+                        if ( !$result['error'] ) {
+                            echo '<wikipage_preview>', $result['content'], '</wikipage_preview>';
+                        } else {
+                            echo '<content_create_failed message="', $result['error'], '"/>';
+                            echo '<syntax>', $result['syntaxCheck'], '</syntax>';
+                        }
+                    }
+                    $title = $thing->title;
+                    $content = htmlspecialchars($content);
+                    section::drawForm($url, $title, TRUE, $content, FALSE, NULL);
                 } else { 
                     echo '<create_failed message="Could not create object ', htmlspecialchars($title), '"/>';
                 }
@@ -216,9 +311,46 @@ class section extends content {
         track('section::method::edit');
         if (hasPermission($this, $wtf->user, 'editGroup')) {    // check permission
             $update = getValue('submit', FALSE);
+            $preview = getValue('preview', FALSE);
+            $small = getValue('small', FALSE);
+            $content = getValue('content', FALSE);
+            $comment = getValue('comment', TRUE);
             $loadtime = getValue('loadtime', NULL);
+            $act = getValue('act', FALSE);
 
-            if ($update) { // update thing
+            if ( $act ) {
+                $key =  getValue('key', FALSE);
+                $content = $this->content;
+                $obj_element = $this->objectlist[$key];
+
+                if ( $key < 0 || $obj_element == NULL ) {
+                    echo '<section_edit_failed '.
+                         'message="Could not edit section elements, invalid element specified."/>';
+                } elseif ( $act == 'remove' ) {
+                    $obj = wtf::loadObject($obj_element['objectid'], 0, $obj_element['classname']);
+                    if ( $this->removeFromSection($obj) ) {
+                        $obj->save(); // save changes if successfully removed from session
+                    }                     
+                } elseif ( $act == 'up' && $key > 0 ) { 
+                    $prev_element = $this->objectlist[$key-1];
+                    $this->objectlist[$key-1] = $obj_element;
+                    $this->objectlist[$key] = $prev_element;
+                    $this->save();
+                } elseif ( $act == 'down' && $key < count($this->objectlist) ) {
+                    $next_element = $this->objectlist[$key+1];
+                    $this->objectlist[$key+1] = $obj_element;
+                    $this->objectlist[$key] = $next_element;
+                    $this->save();
+                }
+            } elseif ($update) { // update thing
+                // Check if we should increment the version
+                $incrementVersion = TRUE;
+                if ( $small == 'on' &&
+                     $wtf->user->objectid == $this->updatorid &&
+                     $wtf->user->objectid != ANONYMOUSUSERID) {
+                    $incrementVersion = FALSE;
+                }
+
                 // Make sure content isn't locked
                 if ( (!isset($loadtime) ||
                       !is_numeric($loadtime) ||
@@ -226,13 +358,38 @@ class section extends content {
                      ($this->updatorid == ANONYMOUSUSERID || 
                       $this->updatorid != $wtf->user->objectid) ) {
                     echo '<section_edit_locked/>';
-                } else {
+                } elseif ($content) {
+                    $result = $this->update($content, $comment, $incrementVersion);
+                    if ( $result['success'] ) {
+                        $this->save();              // save to database
+                        $this->tidyArchive();       // tidy up archived versions
+                        echo '<wikipage_edit_updated message="', $comment, '"/>';
+                    } else {
+                        echo '<content_edit_failed message="', $result['error'], '"/>';
+                        echo '<syntax>', $result['syntaxCheck'], '</syntax>';
+                    }
                 }
+                $content = htmlspecialchars($content);
+            } elseif ($preview) {
+                if ( $content ) {
+                    $result = $this->preview($content);
+                    if ( !$result['error'] ) {
+                        echo '<wikipage_preview>', $result['content'], '</wikipage_preview>';
+                    } else {
+                        echo '<content_edit_failed message="', $result['error'], '"/>';
+                        echo '<syntax>', $result['syntaxCheck'], '</syntax>';
+                    }
+                }
+                $content = htmlspecialchars($content);
+            } else {
+                $content = $this->content;
             }
+            
+            $title = $this->title;
             
             // Draw form, $title is not editable
             $this->drawForm(THINGIDURI.$this->objectid.'&amp;class='.get_class($this).'&amp;op=edit', 
-                            $title, FALSE, $loadtime);
+                            $title, FALSE, $content, $comment, $small, $loadtime);
         } else {
             echo '<thing_permissionerror method="edit" title="'.$this->title.'"/>';
         }
@@ -243,16 +400,11 @@ class section extends content {
 
 // formatting
 $FORMAT = array_merge($FORMAT, array(
-// form
-    'section_form' => '<form method="post" action="{url}">'.
-                      '<input type="hidden" name="{thingidfield}" value="{thingid}" />'.
-                      '<input type="hidden" name="{versionfield}" value="{version}" />'.
-                      '<input type="hidden" name="{loadtimefield}" value="{loadtime}" />',
-    '/section_form' => '<p><input type="submit" name="{submit}" value="Save" /></p></form>',
-    'section_title' => '<p>Editing section "<a href="'.THINGURI.'{title}">{title}</a>" (version {version})</p>',
-    '/section_title' => '',
-    'section_titlebox' => '<p>Title: <input type="text" name="{name}" size="50" maxlength="{maxlength}" value="',
-    '/section_titlebox' => '"/></p>', 
+// view
+    'sectionlist' => '<UL>',
+    '/sectionlist' => '</UL>',
+    'sectionlist_item' => '<LI>',
+    '/sectionlist_item' => '</LI>',
 
 // create
     'section_create_updated' => '<h2 class="success">Create Successful</h2><p>Page created successfully.</p>',
@@ -261,11 +413,25 @@ $FORMAT = array_merge($FORMAT, array(
     '/section_create_permission' => '',
 
 // edit
+    'sectionlist_edit' => '<h3>Section Contents</h3>',
+    '/sectionlist_edit' => '<h3>Section Information</h3><p>The following wiki-formatted text will appear above the section list as descriptive/introductory text.',
+    'sectionlist_edit_item' =>         '',
+    '/sectionlist_edit_item' =>        ' [<a href="{url}&act=remove&key={key}">Remove</a>]<br />'."\n",
+    'sectionlist_edit_item_up' =>      '',
+    '/sectionlist_edit_item_up' =>     ' [<a href="{url}&act=up&key={key}">Up</a>|'.
+                                       '<a href="{url}&act=remove&key={key}">Remove</a>]<br />'."\n",
+    'sectionlist_edit_item_down' =>    '',
+    '/sectionlist_edit_item_down' =>   ' [<a href="{url}&act=down&key={key}">Down</a>|'.
+                                       '<a href="{url}&act=remove&key={key}">Remove</a>]<br />'."\n",
+    'sectionlist_edit_item_updown' =>  '',
+    '/sectionlist_edit_item_updown' => ' [<a href="{url}&act=up&key={key}">Up</a>|'.
+                                         '<a href="{url}&act=down&key={key}">Down</a>|'.
+                                         '<a href="{url}&act=remove&key={key}">Remove</a>]<br />'."\n",
     'section_edit_updated' => '<h2 class="success">Edit Successful</h2><p>Section updated successfully.</p>',
     '/section_edit_updated' => '',
     'section_edit_failed' => '<h2 class="fail">Edit Failed</h2><p>{message}</p>',
     '/section_edit_failed' => '',
-    'section_edit_locked' => '<p class="error">This page is currently being updated by someone else, please review their changes before making your own.</p>',
+    'section_edit_locked' => '<p class="error">This page is currently being updated by someone else, please review their changes before making your own.</p>'."\n",
     '/section_edit_locked' => '',
 
 // diff
