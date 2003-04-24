1c81483d2275b24f9fad8be706cd894464b17f97
graf25 <graf25@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-04-24 02:27:52 +0000

    Added lowsrc to the list of attributes to be removed. Also removing redundant /s switches.

--- squirrelmail/functions/mime.php
+++ squirrelmail/functions/mime.php
@@ -1995,10 +1995,11 @@ function magicHTML($body, $id){
     $rm_attnames = Array(
                          "/.*/" =>
                          Array(
-                               "/target/si",
-                               "/^on.*/si",
-                               "/^dynsrc/si",
-                               "/^data.*/si"
+                               "/target/i",
+                               "/^on.*/i",
+                               "/^dynsrc/i",
+                               "/^data.*/i",
+                               "/^lowsrc/i"
                                )
                          );
 
@@ -2009,10 +2010,10 @@ function magicHTML($body, $id){
                 "/^src|background/i" =>
                     Array(
                           Array(
-                                "|^([\'\"])\s*\.\./.*([\'\"])|si",
-                                "/^([\'\"])\s*\S+\s*script\s*:.*([\'\"])/si",
-                                "/^([\'\"])\s*mocha\s*:*.*([\'\"])/si",
-                                "/^([\'\"])\s*about\s*:.*([\'\"])/si"
+                                "|^([\'\"])\s*\.\./.*([\'\"])|i",
+                                "/^([\'\"])\s*\S+\s*script\s*:.*([\'\"])/i",
+                                "/^([\'\"])\s*mocha\s*:*.*([\'\"])/i",
+                                "/^([\'\"])\s*about\s*:.*([\'\"])/i"
                                 ),
                           Array(
                                 "\\1$secremoveimg\\2",
@@ -2024,10 +2025,10 @@ function magicHTML($body, $id){
                 "/^href|action/i" =>
                     Array(
                           Array(
-                                "|^([\'\"])\s*\.\./.*([\'\"])|si",
-                                "/^([\'\"])\s*\S+\s*script\s*:.*([\'\"])/si",
-                                "/^([\'\"])\s*mocha\s*:*.*([\'\"])/si",
-                                "/^([\'\"])\s*about\s*:.*([\'\"])/si"
+                                "|^([\'\"])\s*\.\./.*([\'\"])|i",
+                                "/^([\'\"])\s*\S+\s*script\s*:.*([\'\"])/i",
+                                "/^([\'\"])\s*mocha\s*:*.*([\'\"])/i",
+                                "/^([\'\"])\s*about\s*:.*([\'\"])/i"
                                 ),
                           Array(
                                 "\\1#\\2",
@@ -2039,13 +2040,13 @@ function magicHTML($body, $id){
                 "/^style/si" =>
                     Array(
                           Array(
-                                "/expression/si",
-                                "/binding/si",
-                                "/behaviou*r/si",
-                                "|url\(([\'\"])\s*\.\./.*([\'\"])\)|si",
-                                "/url\(([\'\"])\s*\S+\s*script\s*:.*([\'\"])\)/si",
-                                "/url\(([\'\"])\s*mocha\s*:.*([\'\"])\)/si",
-                                "/url\(([\'\"])\s*about\s*:.*([\'\"])\)/si"
+                                "/expression/i",
+                                "/binding/i",
+                                "/behaviou*r/i",
+                                "|url\(([\'\"])\s*\.\./.*([\'\"])\)|i",
+                                "/url\(([\'\"])\s*\S+\s*script\s*:.*([\'\"])\)/i",
+                                "/url\(([\'\"])\s*mocha\s*:.*([\'\"])\)/i",
+                                "/url\(([\'\"])\s*about\s*:.*([\'\"])\)/i"
                                ),
                           Array(
                                 "idiocy",
@@ -2065,12 +2066,12 @@ function magicHTML($body, $id){
          * to false.
          */
          array_push($bad_attvals{'/.*/'}{'/^src|background/i'}[0],
-                    '/^([\'\"])\s*https*:.*([\'\"])/si');
+                    '/^([\'\"])\s*https*:.*([\'\"])/i');
          array_push($bad_attvals{'/.*/'}{'/^src|background/i'}[1],
                     "\\1$secremoveimg\\2");
-         array_push($bad_attvals{'/.*/'}{'/^style/si'}[0],
+         array_push($bad_attvals{'/.*/'}{'/^style/i'}[0],
                     '/url\(([\'\"])\s*https*:.*([\'\"])\)/si');
-         array_push($bad_attvals{'/.*/'}{'/^style/si'}[1],
+         array_push($bad_attvals{'/.*/'}{'/^style/i'}[1],
                     "url(\\1$secremoveimg\\2)");
     }
 
