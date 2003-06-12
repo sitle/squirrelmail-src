75ae14cba07c7ac3b4a326b70347734a8c7a1c7d
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-06-12 21:13:11 +0000

    Initial commit of my chaos, so others can see where i was experimenting with.

---
+++ sm2/lib/messages.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * messages.class.inc
+ *
+ * Copyright (c) 2003 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * messages functions.
+ * 
+ *
+ * Author: Marc Groot Koerkamp (Sourceforce username: stekkel) 2003
+ *
+ * $Id$
+ */
+ 
+ class Messages() extends Service {
+ 	var $parser
+ 
+	/* shared functions between nntp, pop3 and imap */
+ 	
+	function sqDeleteMsgs($oMbx,$aIds) {
+		
+ 
+ 
+ }
