e65e5b315707ced2c64113298f54b7d0f117ba66
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-07-07 23:31:50 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1_4_1'.

---
+++ tags/rel-1_4_1/squirrelmail/themes/servery_theme.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * servery_theme.php
+ *    Name:    Servery.cz Theme
+ *    Author:  Ondrej Sury <ondrej@globe.cz>
+ *    Date:    August 18, 2000
+ *    Comment: 
+ *
+ * Copyright (c) 2000-2003 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * $Id$
+ */
+
+global $color;
+$color[0]   = '#F1EEE3'; // (light gray)     TitleBar
+$color[1]   = '#800000'; // (red)
+$color[2]   = '#CC0000'; // (light red)      Warning/Error Messages
+$color[3]   = '#7D6F3F'; // (green-blue)     Left Bar Background
+$color[4]   = '#FFFFFF'; // (white)          Normal Background
+$color[5]   = '#C7B98B'; // (light yellow)   Table Headers
+$color[6]   = '#000000'; // (black)          Text on left bar
+$color[7]   = '#434031'; // (blue)           Links
+$color[8]   = '#000000'; // (black)          Normal text
+$color[9]   = '#C7B98B'; // (mid-gray)       Darker version of #0
+$color[10]  = '#F1EEE3'; // (dark gray)      Darker version of #9
+$color[11]  = '#770000'; // (dark red)       Special Folders color
+$color[15]  = '#C7B98B'; // (mid-gray)       Unselectable folders
+?>
