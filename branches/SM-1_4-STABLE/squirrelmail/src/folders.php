95f52861ca3b13d1a078f933241ca238ededc69f
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-09-02 21:34:04 +0000

    force non cached retrieval of the mailbox-tree

--- branches/SM-1_4-STABLE/squirrelmail/src/folders.php
+++ branches/SM-1_4-STABLE/squirrelmail/src/folders.php
@@ -85,7 +85,9 @@ if ( isset($success) && $success ) {
 echo "\n<br>";
 
 $imapConnection = sqimap_login ($username, $key, $imapServerAddress, $imapPort, 0);
-$boxes = sqimap_mailbox_list($imapConnection);
+
+// force retrieval of a non cached folderlist
+$boxes = sqimap_mailbox_list($imapConnection,true);
 
 /** CREATING FOLDERS **/
 echo html_tag( 'table', '', 'center', '', 'width="70%" cellpadding="4" cellspacing="0" border="0"' ) .
