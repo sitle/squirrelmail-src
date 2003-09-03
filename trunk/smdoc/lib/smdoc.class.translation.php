5947eab0daf242e049ce98fa23179b6d58e13313
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-09-03 05:41:40 +0000

    all kinds of crap. too much to enumerate. lots of tweaks... customizing the hell out of the user class..

--- trunk/smdoc/lib/smdoc.class.translation.php
+++ trunk/smdoc/lib/smdoc.class.translation.php
@@ -27,136 +27,152 @@ setConst('TRANSLATION_DEFAULT_LANGUAGE', 'en_US');
 //setConst('TRANSLATION_DEFAULT_LANGUAGE_ICON', 'en_US');
 
 /** CLASS DECLARATION **/
-class foowd_translation extends foowd_workspace {
+class smdoc_translation extends foowd_workspace {
 
     var $language_icon;
 
-    /**
-     * Constructor
-     */
-    function foowd_translation( &$foowd,
-                                $title = NULL,
-                                $description = NULL,
-                                $viewGroup = NULL,
-                                $adminGroup = NULL,
-                                $deleteGroup = NULL,
-                                $enterGroup = NULL,
-                                $icon = NULL )
-    {
-        $foowd->track('foowd_translation->constructor');
+  /**
+   * Constructor
+   */
+  function smdoc_translation( &$foowd,
+                              $title = NULL,
+                              $description = NULL,
+                              $viewGroup = NULL,
+                              $adminGroup = NULL,
+                              $deleteGroup = NULL,
+                              $enterGroup = NULL,
+                              $icon = NULL )
+  {
+    $foowd->track('smdoc_translation->constructor');
 
-        /* set object vars */
-        $this->language_icon = $icon;
-        $this->initialize();
+    /* set object vars */
+    $this->language_icon = $icon;
+    $this->initialize();
 
-        $foowd->track();
-    }
+    $foowd->track();
+  }
 
 /*** STATIC METHODS ***/
 
-    /**
-     * STATIC
-     * Initializes array containing objectid, icon, and language code
-     * for each defined translation
-     */
-    function initialize(&$foowd, $forceRefresh = FALSE)
+  /**
+   * Initializes array containing objectid, icon, and language code
+   * for each defined translation
+   *
+   * @param object foowd The foowd environment object.
+   * @param boolean forceRefresh Force refresh of session cache.
+   */
+  function initialize(&$foowd, $forceRefresh = FALSE)
+  {
+    $foowd->track('smdoc_translation::initialize', $forceRefresh);
+
+    $default_title = getConstOrDefault('TRANSLATION_DEFAULT_LANGUAGE', 'en_US');
+    $default_icon = getConstOrDefault('TRANSLATION_DEFAULT_LANGUAGE_ICON', '');
+
+    $session_links = new input_session('lang_links', NULL);
+    $session_langs = new input_session('languages', NULL);
+
+    if ( isset($session_links->value) && 
+         isset($session_langs->value) && !$forceRefresh )
     {
-      $default_title = getConstOrDefault('TRANSLATION_DEFAULT_LANGUAGE', 'en_US');
-      $default_icon = getConstOrDefault('TRANSLATION_DEFAULT_LANGUAGE_ICON', '');
+      $foowd->track();
+      return;
+    }
 
-      $session_links = new input_session('lang_links', NULL);
-      $session_langs = new input_session('languages', NULL);
+    $links = array();
+    $languages = array();
 
-      if ( isset($session_links->value) && 
-           isset($session_langs->value) && !$forceRefresh )
-        return;
+    $url = getURI(array()). '?class=foowd_translation&method=enter&langid=';
 
-      $links = array();
-      $languages = array();
+    $the_url = '<a href="' . $url . '0">';
+    if ( $default_icon != '' )
+    {
+      $the_url .= '<img src="' . $default_icon . '" ';
+      $the_url .= 'alt="' . $default_title . '" border="0" />';
+    }
+    else
+      $the_url .= $default_title;
+    $the_url .=  '</a>';
 
-      $url = getURI(array()). '?class=foowd_translation&method=enter&langid=';
+    $links[0] = $the_url;
+    $languages[0] = $default_title;
 
-      $the_url = '<a href="' . $url . '0">';
-      if ( $default_icon != '' )
+    $t_objects = $foowd->retrieveObjects(
+                         array('classid = '.TRANSLATION_CLASS_ID),
+                         NULL,
+                         array('title'));
+
+    while ($trans_obj = $foowd->retrieveObject($t_objects))
+    {
+      $the_url = '<a href="' .$url. $trans_obj->objectid. '">';
+      if ( isset($trans_obj->language_icon) )
       {
-        $the_url .= '<img src="' . $default_icon . '" ';
-        $the_url .= 'alt="' . $default_title . '" border="0" />';
+        $the_url .= '<img src="' . $trans_obj->language_icon . '" ';
+        $the_url .= 'alt="' . $trans_obj->title . '" border="0" />';
       }
       else
-        $the_url .= $default_title;
+        $the_url .= $trans_obj->title;
       $the_url .=  '</a>';
 
-      $links[0] = $the_url;
-      $languages[0] = $default_title;
+      $links[$trans_obj->objectid] = $the_url;
+      $languages[$trans_obj->objectid] = $trans_obj->title;
+      unset($trans_obj);
+    }
+    $session_links->set($links);
+    $session_langs->set($languages);
 
-      $t_objects = $foowd->retrieveObjects(
-                            array('classid = '.TRANSLATION_CLASS_ID),
-                            NULL,
-                            array('title'));
+    $foowd->track();
+  }
 
-      while ($trans_obj = $foowd->retrieveObject($t_objects))
-      {
-        $the_url = '<a href="' .$url. $trans_obj->objectid. '">';
-        if ( isset($trans_obj->language_icon) )
-        {
-          $the_url .= '<img src="' . $trans_obj->language_icon . '" ';
-          $the_url .= 'alt="' . $trans_obj->title . '" border="0" />';
-        }
-        else
-          $the_url .= $trans_obj->title;
-        $the_url .=  '</a>';
-
-        $links[$trans_obj->objectid] = $the_url;
-        $languages[$trans_obj->objectid] = $trans_obj->title;
-        unset($trans_obj);
-      }
-      $session_links->set($links);
-      $session_langs->set($languages);
+  /** 
+   * Retrieve url for given object id.
+   * if objectid is NULL, return array containing all URLs
+   * 
+   * @param object foowd The foowd environment object.
+   * @param optional int objectid Specific translation to retrieve.
+   * @return URL for specified translation, or array of all translations.
+   */
+  function getLink(&$foowd, $objectid = FALSE)
+  {
+    $session_links = new input_session('lang_links', NULL);
+    if ( !isset($session_links->value) ) {
+      foowd_translation::initialize($foowd);
+      $session_links->refresh();
     }
 
-    /** STATIC
-     * Retrieve url for given object id.
-     * if objectid is NULL, return array containing all URLs
-     */
-    function getLink(&$foowd, $objectid = FALSE)
+    if ($objectid === FALSE)
+      return $session_links->value;
+
+    if ( isset($session_links->value[$objectid]) )
+      return $session_links->value[$objectid];
+    else
+      return NULL;
+  }
+
+  /** 
+   * Retrieve given language.
+   * if objectid is NULL, return array containing all languages
+   * 
+   * @param object foowd The foowd environment object.
+   * @param optional int objectid Specific translation to retrieve.
+   * @return specified language string, or array of all languages.
+   */
+  function getLanguage(&$foowd, $objectid = FALSE)
+  {
+    $session_langs = new input_session('languages', NULL);
+    if ( !isset($session_langs->value) ) 
     {
-      $session_links = new input_session('lang_links', NULL);
-      if ( !isset($session_links->value) ) {
-        foowd_translation::initialize($foowd);
-        $session_links->refresh();
-      }
-
-      if ($objectid === FALSE)
-        return $session_links->value;
-
-      if ( isset($session_links->value[$objectid]) )
-        return $session_links->value[$objectid];
-      else
-        return NULL;
+      foowd_translation::initialize($foowd);
+      $session_langs->refresh();
     }
 
+    if ($objectid === FALSE)
+      return $session_langs->value;
 
-    /** STATIC
-     * Retrieve url for given object id.
-     * if objectid is NULL, return array containing all URLs
-     */
-    function getLanguage(&$foowd, $objectid = FALSE)
-    {
-      $session_langs = new input_session('languages', NULL);
-      if ( !isset($session_langs->value) ) {
-        foowd_translation::initialize($foowd);
-        $session_langs->refresh();
-      }
-
-      if ($objectid === FALSE)
-        return $session_langs->value;
-
-      if ( isset($session_langs->value[$objectid]) )
-        return $session_langs->value[$objectid];
-      else
-        return NULL;
-    }
-
+    if ( isset($session_langs->value[$objectid]) )
+      return $session_langs->value[$objectid];
+    else
+      return NULL;
+  }
 
 /*** CLASS METHODS ***/
 
