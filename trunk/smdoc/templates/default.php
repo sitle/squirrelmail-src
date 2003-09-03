f1c16a582cd99fdd55f001a0671c49195cb220c6
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-09-03 05:37:07 +0000

    updates to display templates

--- trunk/smdoc/templates/default.php
+++ trunk/smdoc/templates/default.php
@@ -1,7 +1,16 @@
 <?php
 
-foreach ($t as $item) {
-	echo '<p>', $item, '</p>';
+function default_body($foowd, $className, $method, $user, $object, $t)
+{
+    echo '<h3>Default Object Display</h3>';
+    if ( isset($object) )
+    {
+      echo '<pre>Class: ', $className, "\n", 'Method: ',  $method, '</pre>';
+      echo '<div class="debug_output_heading">', _("Contents of Current Object"), '</div>';
+      show($object);
+    }
 }
 
+$t['body_function'] = 'default_body';
+include($foowd->template.'/index.php');
 ?>
