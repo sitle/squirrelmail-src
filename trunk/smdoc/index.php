85a0b825e45d02d521cfc0c238f60c75e5e993b6
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-09-30 05:06:15 +0000

    updates for latest foowd changes (templating)

--- trunk/smdoc/index.php
+++ trunk/smdoc/index.php
@@ -1,9 +1,9 @@
 <?php
 
-require('config.foowd.php');             // include config and Foowd functions
+require('config/config.foowd.php');             // include config and Foowd functions
 
 // init Foowd
-$foowd = new smdoc(NULL, NULL, $DEFAULT_GROUPS);
+$foowd = new smdoc(NULL, NULL, $DEFAULT_GROUPS, NULL, 'lib');
 
 // get object details
 include_once($foowd->path.'/input.querystring.php');
@@ -32,48 +32,52 @@ $objectid = $objectid->value;
 $classid = $classid->value;
 $version = $version->value;
 $method = $method->value;
+$result = FALSE;
 
-if (isset($objectid)) { // fetch object and call object method
-    $foowd->debug('msg', 'fetch and call object method');
+if (isset($objectid))  // fetch object and call object method
+{
+  $foowd->debug('msg', 'fetch and call object method');
 
-    if ( !isset($method) )
-        $method = getConstOrDefault('DEFAULT_METHOD','view');
+  if ( !isset($method) )
+    $method = getConstOrDefault('DEFAULT_METHOD','view');
 
-	if ($object = $foowd->fetchObject($objectid, $classid, $version, $method)) {
-		if (is_object($object)) {
-			$className = getClassName($object->classid);
-		} else {
-			$className = getClassName($classid);
-		}
-		$t = $foowd->method($object, $method);
-        $methodName = 'object_'.$method;
-	} else {
-		trigger_error('Object not found: ' . 
-                 (isset($objectName->value) ? $objectName->value : $objectid) . 
-                 ' (' . $className . ')', E_USER_NOTICE);
-	}
-} else { // call class method
-    $foowd->debug('msg', 'fetch and call class method');
-	if ( !isset($className) )
-        $className = getClassName($classid);
-    if ( !isset($method) )
-        $method = getConstOrDefault('DEFAULT_CLASS_METHOD', 'create');
+  if ($object = $foowd->fetchObject($objectid, $classid, $version, $method)) 
+  {
+    if (is_object($object)) 
+      $className = getClassName($object->classid);
+    else 
+      $className = getClassName($classid);
 
-	$t = $foowd->method($className, $method);
-    $methodName = 'class_'.$method;
+    $result = $foowd->method($object, $method);
+    $methodName = 'object_'.$method;
+  }
+  else 
+  {
+    trigger_error('Object not found: ' 
+                  . (isset($objectName->value) ?$objectName->value : $objectid)
+                  . ' (' . $className . ')', E_USER_ERROR);
+  }
+} 
+else  // call class method
+{
+  $foowd->debug('msg', 'fetch and call class method');
+
+  if ( !isset($className) )
+    $className = getClassName($classid);
+
+  if ( !isset($method) )
+    $method = getConstOrDefault('DEFAULT_CLASS_METHOD', 'create');
+
+  $result = $foowd->method($className, $method);
+  $methodName = 'class_'.$method;
 }
 
 $foowd->debug('msg', 'display result using template');
-if (is_array($t)) {
-    if ( isset($objectid) && $method != 'delete' )
-        $t['showurl'] = true;
-    else
-        $t['showurl'] = false;
-	include($foowd->getTemplateName($className, $methodName));
-} else {
-	trigger_error($t, E_USER_NOTICE);
-}
 
+if ( $result ) 
+  $foowd->template->display($foowd->getTemplateName($className, $methodName));
+else 
+  trigger_error("Previous error, no defined result", E_USER_NOTICE);
 
 // destroy Foowd
 $foowd->destroy();
