dc6c57db83a04b3468fa5e2c2ffdd130b286772d
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-11-03 23:58:40 +0000

    EIMS workaround. EIMS returns the SEARCH response as multiple untagged SEARCH responses. That is against RFC3501 !!! example:
    A005 UID SEARCH UID 1:*
    * SEARCH 5949 8160 8401 8402 8413 8470 8471 8472 8497 8499
    * SEARCH 8502 8504 8514
    A005 OK SEARCH completed
    
    We only use that call in case of non server side sorting.

--- branches/SM-1_4-STABLE/squirrelmail/functions/imap_messages.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/imap_messages.php
@@ -199,6 +199,14 @@ function sqimap_get_sort_order ($imap_stream, $sort, $mbxresponse) {
 }
 
 
+/**
+ * Get sort order from server if server does not have the SORT extension
+ * and return it as array for mailbox_display.
+ *
+ * @param  resource $imap_stream
+ * @param  array    $mbxresponse response from a sqimap_mailbox_select
+ * @return array    $php_sort_array
+ */
 function sqimap_get_php_sort_order ($imap_stream, $mbxresponse) {
     global $uid_support;
 
@@ -217,8 +225,12 @@ function sqimap_get_php_sort_order ($imap_stream, $mbxresponse) {
         $query = "SEARCH UID 1:$uidnext";
         $uids = sqimap_run_command ($imap_stream, $query, true, $response, $message, true);
         if (isset($uids[0])) {
-            if (preg_match("/^\* SEARCH (.+)$/", $uids[0], $regs)) {
-                $php_sort_array = preg_split("/ /", trim($regs[1]));
+            $php_sort_array = array();
+            // EIMS workaround. EIMS returns the result as multiple untagged SEARCH responses
+            foreach($uids as $line) {
+                if (preg_match("/^\* SEARCH (.+)$/", $line, $regs)) {
+                     $php_sort_array += preg_split("/ /", trim($regs[1]));
+                }
             }
         }
         if (!preg_match("/OK/", $response)) {
@@ -244,8 +256,8 @@ function get_parent_level ($imap_stream) {
         $child = "";
         $cutoff = 0;
 
-    /* loop through the threads and take unwanted characters out 
-       of the thread string then chop it up 
+    /* loop through the threads and take unwanted characters out
+       of the thread string then chop it up
      */
     for ($i=0;$i<count($thread_new);$i++) {
         $thread_new[$i] = preg_replace("/\s\(/", "(", $thread_new[$i]);
