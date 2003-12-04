e733912918f352442ffe75a64f0e52e966367879
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-12-04 18:03:51 +0000

    Preliminary doc blocks Foowd-base environment classes and libraries

--- trunk/smdoc/lib/lib.php
+++ trunk/smdoc/lib/lib.php
@@ -19,20 +19,18 @@ along with Foowd; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-/*
+/**
+ * Basic functions commonly used throughout the framework.
+ *
  * Modified by SquirrelMail Development
  * $Id$
+ * 
+ * @package Foowd
  */
-
-/*
-lib.php
-Foowd function library
-*/
 
 /**
  * Display a variable in a formatted way.
  *
- * @package Foowd/Lib
  * @param mixed var Variable to output.
  */
 function show($var) 
@@ -64,97 +62,92 @@ function show($var)
 /**
  * Return the value of a constant or a default value.
  *
- * @package Foowd/Lib
  * @param str constant Name of a constant.
  * @param mixed default Default value.
  * @return mixed The value of the constant or the default value.
  */
-function getConstOrDefault($constant, $default) {
-  if (defined($constant)) {
+function getConstOrDefault($constant, $default) 
+{
+  if (defined($constant))
     return constant($constant);
-  } else {
-    return $default;
-  }
+
+  return $default;
 }
 
 /**
  * Set a constant to a value if the constant is defined.
  *
- * @package Foowd/Lib
  * @param str constName Name of a constant.
  * @param mixed value The value to set.
  */
-function setConst($constName, $value) {
+function setConst($constName, $value) 
+{
   $constName = strtoupper($constName);
-  if (!defined($constName)) {
+  if (!defined($constName))
     define($constName, $value);
-  }
 }
 
 /**
  * Find the max length of string allowed by a regex.
  *
- * @package Foowd/Lib
  * @param str regex The regular expression to find the length of.
  * @param int default The default length to return if we can not find a length in the regular expression.
  * @return str The maximum length allowed by a regular expression.
  */
-function getRegexLength($regex, $default) {
-  if (preg_match('/\*/', $regex)) {
+function getRegexLength($regex, $default) 
+{
+  if (preg_match('/\*/', $regex))
     return 0;
-  } elseif (preg_match('/\+/', $regex)) {
+  if (preg_match('/\+/', $regex))
     return 0;
-  } elseif (preg_match('/\{[0-9,]*([0-9]+)\}/U', $regex, $results = array())) {
+  if (preg_match('/\{[0-9,]*([0-9]+)\}/U', $regex, $results = array()))
     return $results[1];
-  } elseif (preg_match('/\?/', $regex)) {
+  if (preg_match('/\?/', $regex))
     return 1;
-  } else {
-    return $default;
-  }
+
+  return $default;
 }
 
 /**
  * Get the user group permission of a object or class method.
  *
- * @package Foowd/Lib
  * @param str className Name of the class the method belongs to.
  * @param str methodName Name of the method.
  * @param str type Type of method, 'class' or 'object'.
  * @return str The user group string the permission is set to.
  */
-function getPermission($className, $methodName, $type = '') {
+function getPermission($className, $methodName, $type = '') 
+{
   $type = strtoupper($type);
-  if ( $type != 'CLASS' ) {
+  if ( $type != 'CLASS' )
     $type = 'OBJECT';
-  }
 
   if ( !isset($className) || !class_exists($className) )
     $className = 'foowd_object';
 
   $constName = 'PERMISSION_'.strtoupper($className).'_'.$type.'_'.strtoupper($methodName);
-  if (defined($constName)) {
+  if (defined($constName))
     return constant($constName);
-  } elseif ($className == 'foowd_object') { // none found
+  if ($className == 'foowd_object') // none found
     return 'Everyone';
-  } else { // look at parent
-    return getPermission(get_parent_class($className), $methodName, $type);
-  }
+
+  // Recurse to look at permissions of parent    
+  return getPermission(get_parent_class($className), $methodName, $type);
 }
 
 /**
  * Set the user group permission of a object or class method.
  *
- * @package Foowd/Lib
  * @param str className Name of the class the method belongs to.
  * @param str type Type of method, 'class' or 'object'.
  * @param str methodName Name of the method.
  * @param str value User group string to set the permission to.
  */
-function setPermission($className, $type, $methodName, $value = 'Everyone') {
+function setPermission($className, $type, $methodName, $value = 'Everyone') 
+{
   $type = strtoupper($type);
-  if ( $type != 'CLASS' ) {
+  if ( $type != 'CLASS' )
     $type = 'OBJECT';
-  }
 
   setConst('PERMISSION_'.strtoupper($className).'_'.$type.'_'.strtoupper($methodName), $value);
 }
@@ -162,11 +155,11 @@ function setPermission($className, $type, $methodName, $value = 'Everyone') {
 /**
  * Set class meta data.
  *
- * @package Foowd/Lib
  * @param str className Name of the class.
  * @param str description Text description of the class.
  */
-function setClassMeta($className, $description) {
+function setClassMeta($className, $description) 
+{
   $classid = crc32(strtolower($className));
   setConst('META_'.$classid.'_CLASSNAME', $className);
   setConst('META_'.$classid.'_DESCRIPTION', $description);
@@ -176,80 +169,76 @@ function setClassMeta($className, $description) {
 /**
  * Get class name given the classid.
  *
- * @package Foowd/Lib
  * @param int classid The id of the class.
  * @return str The class name.
  */
-function getClassName($classid) {
-  if (defined('META_'.$classid.'_CLASSNAME')) {
+function getClassName($classid) 
+{
+  if (defined('META_'.$classid.'_CLASSNAME'))
     return constant('META_'.$classid.'_CLASSNAME');
-  } else {
-    trigger_error('Could not find class name from class ID '.$classid);
-  }
+
+  trigger_error('Could not find class name from class ID '.$classid);
 }
 
 /**
  * Get class description given the classid.
  *
- * @package Foowd/Lib
  * @param int classid The id of the class.
  * @return str The class description.
  */
-function getClassDescription($classid) {
-  if (defined('META_'.$classid.'_DESCRIPTION')) {
+function getClassDescription($classid) 
+{
+  if (defined('META_'.$classid.'_DESCRIPTION'))
     return constant('META_'.$classid.'_DESCRIPTION');
-  } else {
-    return 'Unknown';
-  }
+
+  return 'Unknown';
 }
 
 /**
  * Whether a class has been loaded. Uses the class meta data to see if the given classid has been loaded into the system.
  *
- * @package Foowd/Lib
  * @param int classid The id of the class.
  * @return bool TRUE if the class is loaded.
  */
-function classLoaded($classid = NULL) {
-  if ($classid == NULL || defined('META_'.$classid.'_CLASSNAME')) {
+function classLoaded($classid = NULL) 
+{
+  if ($classid == NULL || defined('META_'.$classid.'_CLASSNAME'))
     return TRUE;
-  } else {
-    return FALSE;
-  }
+
+  return FALSE;
 }
 
 /**
  * Whether a class is a Foowd object.
  *
- * @package Foowd/Lib
  * @param str className The name of the class.
  * @return bool TRUE if the class is a child of foowd_object.
  */
-function isFoowdObject($className) {
-  if ($className == 'foowd_object') {
+function isFoowdObject($className) 
+{
+  if ($className == 'foowd_object')
     return TRUE;
-  }
+
   $parentName = get_parent_class($className);
-  if ($parentName == 'foowd_object') {
+  if ($parentName == 'foowd_object')
     return TRUE;
-  } elseif ($parentName != '') {
+  if ($parentName != '')
     return isFoowdObject($parentName);
-  } else {
-    return FALSE;
-  }
+
+  return FALSE;
 }
 
 /**
  * Get the names of all Foowd classes loaded.
  *
- * @package Foowd/Lib
  * @return array An array of class names.
  */
-function getFoowdClassNames() {
-  foreach (get_declared_classes() as $className) {
-    if (isFoowdObject($className)) {
+function getFoowdClassNames() 
+{
+  foreach (get_declared_classes() as $className) 
+  {
+    if (isFoowdObject($className))
       $items[strval(crc32(strtolower($className)))] = $className;
-    }
   }
   return $items;
 }
@@ -257,17 +246,16 @@ function getFoowdClassNames() {
 /**
  * Get system temporary directory.
  *
- * @package Foowd/Lib
  * @return str The system temporary directory.
  */
-function getTempDir() {
-  if (isset($_ENV['TMP'])) {
+function getTempDir() 
+{
+  if (isset($_ENV['TMP'])) 
     return $_ENV['TMP'].'\\';
-  } elseif (isset($_ENV['TEMP'])) {
+  if (isset($_ENV['TEMP']))
     return $_ENV['TEMP'].'\\';
-  } else {
-    return substr($_SERVER['SCRIPT_FILENAME'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/')).'/temp/';
-  }
+
+  return substr($_SERVER['SCRIPT_FILENAME'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/')).'/temp/';
 }
 
 /* URI functions */
@@ -275,21 +263,22 @@ function getTempDir() {
 /**
  * Generate a URI given certain parameters.
  *
- * @package Foowd/Lib
  * @param array parameters An array of querystring value pairs.
  * @return str A URI.
  */
-function getURI($parameters = NULL, $relative = TRUE) { 
-
+function getURI($parameters = NULL, $relative = TRUE) 
+{ 
   $uri = $relative ? '' : 
          (defined('BASE_URL') ? BASE_URL : 'http://'.$_SERVER['HTTP_HOST']);
 
   $uri .= defined('FILENAME') ? FILENAME : $_SERVER['PHP_SELF'] ;
 
-  if (is_array($parameters) && count($parameters) > 0 ) {
+  if (is_array($parameters) && count($parameters) > 0 ) 
+  {
     $i = 0;
     $uri .= '?';
-    foreach ($parameters as $name => $value) {
+    foreach ($parameters as $name => $value) 
+    {
       if ( $i++ > 0 )
         $uri .= '&';
       $uri .= $name.'='.$value;
@@ -303,12 +292,13 @@ function getURI($parameters = NULL, $relative = TRUE) {
 /**
  * Anti-spam an e-mail address.
  *
- * @package Foowd/Lib
  * @param str emailAddress E-mail address to mangle.
  * @return str Munged e-mail address.
  */
-function mungEmail($emailAddress) {
-  switch (rand(1, 4)) {
+function mungEmail($emailAddress) 
+{
+  switch (rand(1, 4)) 
+  {
     case 1:
       return strtr($emailAddress, array('@' => ' at ', '.' => ' dot '));
     case 2:
@@ -326,27 +316,26 @@ function mungEmail($emailAddress) {
 /**
  * Return the time since a certain time in the past.
  *
- * @package Foowd/Lib
  * @param int time The time in the past as a Unix timestamp.
  * @return str A string representation of roughly how long has passed.
  */
-function timeSince($time) {
+function timeSince($time) 
+{
   $time = time() - $time;
-  if ($num = round($time / (3600 * 24 * 365), 1) AND $num > 1) {
+  if ($num = round($time / (3600 * 24 * 365), 1) AND $num > 1) 
     return $num.' years';
-  } elseif ($num = round($time / (3600 * 24 * 30), 1) AND $num > 1) {
+  if ($num = round($time / (3600 * 24 * 30), 1) AND $num > 1) 
     return $num.' months';
-  } elseif ($num = round($time / (3600 * 24 * 7), 1) AND $num > 1) {
+  if ($num = round($time / (3600 * 24 * 7), 1) AND $num > 1) 
     return $num.' weeks';
-  } elseif ($num = round($time / (3600 * 24), 1) AND $num > 1) {
+  if ($num = round($time / (3600 * 24), 1) AND $num > 1) 
     return $num.' days';
-  } elseif ($num = round($time / (3600), 1) AND $num > 1) {
+  if ($num = round($time / (3600), 1) AND $num > 1) 
     return $num.' hours';
-  } elseif ($num = floor($time / 60) AND $num > 1) {
+  if ($num = floor($time / 60) AND $num > 1) 
     return $num.' minutes';
-  } else {
-    return $time.' seconds';
-  }
+
+  return $time.' seconds';
 }
 
 /* E-mail function */
@@ -355,8 +344,7 @@ function timeSince($time) {
  * Send an e-mail. This function is a wrapper to the PHP mail function that
  * includes writing debugging data to the debug stream.
  *
- * @package Foowd/Lib
- * @param object foowd The foowd environment object.
+ * @param smdoc foowd Reference to the foowd environment object.
  * @param str to The e-mail address to send the e-mail to.
  * @param str subject The subject of the e-mail.
  * @param str message The message to send.
@@ -364,8 +352,10 @@ function timeSince($time) {
  * @param str para Additional e-mail parameters.
  * @return bool TRUE on success.
  */
-function email(&$foowd, $to, $subject, $message, $headers = NULL, $para = NULL) {
-  if ($foowd->debug) {
+function email(&$foowd, $to, $subject, $message, $headers = NULL, $para = NULL) 
+{
+  if ($foowd->debug) 
+  {
     $foowd->debug('msg', 'Sending e-mail:');
     $foowd->debug('msg', 'To: '.$to);
     $foowd->debug('msg', 'Subject: '.$subject);
@@ -384,16 +374,16 @@ function email(&$foowd, $to, $subject, $message, $headers = NULL, $para = NULL) 
  * This function sends a test cookie to the client which can be used by
  * {@link cookieTest} to check that the client has cookie support availble.
  *
- * @package Foowd/Lib
- * @param object foowd The foowd environment object.
+ * @param smdoc foowd Reference to the foowd environment object.
  */
-function sendTestCookie(&$foowd) {
-  if ($foowd->cookie_test) {
+function sendTestCookie(&$foowd) 
+{
+  if ($foowd->cookie_test) 
+  {
     include_once(INPUT_DIR.'input.cookie.php');
     $cookieTest = new input_cookie('test', '/^[12]$/', 2);
-    if ($cookieTest->value == 2) {
+    if ($cookieTest->value == 2)
       $cookieTest->set(1);
-    }
   }
 }
 
@@ -403,29 +393,33 @@ function sendTestCookie(&$foowd) {
  * This function looks for the test cookie sent by {@link sendTestCookie} to
  * check that the client has cookie support availble.
  *
- * @package Foowd/Lib
- * @param object foowd The foowd environment object.
+ * @param smdoc foowd Reference to the foowd environment object.
  * @return bool TRUE if cookie support was detected.
  */
-function cookieTest(&$foowd) {
-  if ($foowd->cookie_test && $foowd->user_authType == 'cookie') {
+function cookieTest(&$foowd) 
+{
+  if ($foowd->cookie_test && $foowd->user_authType == 'cookie') 
+  {
     include_once(INPUT_DIR.'input.cookie.php');
     $cookieTest = new input_cookie('test', '/^[12]$/', 2);
-    if ($cookieTest->value == 2) {
+    if ($cookieTest->value == 2)
       return FALSE;
-    }
   }
   return TRUE;
 }
 
 /* Locale functions */
 
-if (!function_exists('_')) { // no gettext support, so define our own "_" function
+/** 
+ * If locale support does not exist, 
+ * define our own "_" function.
+ */
+if (!function_exists('_')) 
+{
   /**
-   * Fake Getext function. If Gettext support is not availble, this function
+   * Fake Getext function. If Gettext support is not available, this function
    * acts as a passthru function in its place.
    *
-   * @package Foowd/Lib
    * @param str text Text to return
    * @return str The returned text.
    */
