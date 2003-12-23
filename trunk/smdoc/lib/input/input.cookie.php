9f7cf16b77ff41c6121839b8040666c90d1fec42
ebullient <ebullient@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2003-12-23 01:38:44 +0000

    make input_cookie a subclass of input_base

--- trunk/smdoc/lib/input/input.cookie.php
+++ trunk/smdoc/lib/input/input.cookie.php
@@ -29,6 +29,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * @subpackage input
  */
 
+/** Include base input library functions and input base class */
+require_once(INPUT_DIR . 'input.lib.php');
+
 /** Define constants for managing cookie behavior */
 if (!defined('COOKIE_EXPIRE')) define('COOKIE_EXPIRE', 31536000);
 if (!defined('COOKIE_PATH')) define('COOKIE_PATH', '');
@@ -45,116 +48,96 @@ if (!defined('COOKIE_SECURE')) define('COOKIE_SECURE', '');
  * @subpackage input
  * @author Paul James
  */
-class input_cookie 
+class input_cookie extends input_base 
 {
-	/**
-	 * The name of the cookie object.
-	 *
-	 * @var string
-	 */
-	var $name;
-	
-	/**
-	 * The value of the cookie.
-	 *
-	 * @var string
-	 */
-	var $value = NULL;
+  /**
+   * How long til the cookie expires in seconds.
+   *
+   * @var int
+   */
+  var $expire;
 
-	/**
-	 * The regular expression used to validate the cookies value.
-	 *
-	 * @var string
-	 */
-	var $regex = NULL;
+  /**
+   * The path the cookie is valid for.
+   *
+   * @var string
+   */
+  var $path;
 
-	/**
-	 * How long til the cookie expires in seconds.
-	 *
-	 * @var int
-	 */
-	var $expire;
+  /**
+   * The domain the cookie is valid for.
+   *
+   * @var string
+   */
+  var $domain;
 
-	/**
-	 * The path the cookie is valid for.
-	 *
-	 * @var string
-	 */
-	var $path;
+  /**
+   * Whether the cookie should only be sent over secure HTTP.
+   *
+   * @var bool
+   */
+  var $secure;
 
-	/**
-	 * The domain the cookie is valid for.
-	 *
-	 * @var string
-	 */
-	var $domain;
+  /**
+   * Constructs a new cookie object.
+   *
+   * @param string name The name of the querystring object.
+   * @param string regex The validation regular expression.
+   * @param string value The initial contents value.
+   * @param int expire How long til the cookie expires in seconds.
+   * @param string path The path the cookie is valid for.
+   * @param string domain The domain the cookie is valid for.
+   * @param bool secure Whether the cookie should only be sent over secure HTTP.
+   */
+  function input_cookie($name, $regex = NULL, $value = NULL, 
+                        $expire = COOKIE_EXPIRE, 
+                        $path = COOKIE_PATH, 
+                        $domain = COOKIE_DOMAIN, 
+                        $secure = COOKIE_SECURE) 
+  {
+    parent::input_base($name, $regex, $value, FALSE, SQ_COOKIE);
 
-	/**
-	 * Whether the cookie should only be sent over secure HTTP.
-	 *
-	 * @var bool
-	 */
-	var $secure;
+    $this->expire = $expire;
+    $this->path = $path;
+    $this->domain = $domain;
+    $this->secure = $secure;
+  }
 
-	/**
-	 * Constructs a new cookie object.
-	 *
-	 * @param string name The name of the querystring object.
-	 * @param string regex The validation regular expression.
-	 * @param string value The initial contents value.
-	 * @param int expire How long til the cookie expires in seconds.
-	 * @param string path The path the cookie is valid for.
-	 * @param string domain The domain the cookie is valid for.
-	 * @param bool secure Whether the cookie should only be sent over secure HTTP.
-	 */
-	function input_cookie($name, $regex = NULL, $value = NULL, $expire = COOKIE_EXPIRE, $path = COOKIE_PATH, $domain = COOKIE_DOMAIN, $secure = COOKIE_SECURE) {
-		$this->name = $name;
-		$this->regex = $regex;
-		$this->expire = $expire;
-		$this->path = $path;
-		$this->domain = $domain;
-		$this->secure = $secure;
-		if (isset($_COOKIE[$name]) && ($this->regex == NULL || preg_match($this->regex, $_COOKIE[$name]))) {
-			if (get_magic_quotes_gpc()) {
-				$this->value = stripslashes($_COOKIE[$name]);
-			} else {
-				$this->value = $_COOKIE[$name];
-			}
-		}
-		if (!isset($this->value)) {
-			$this->value = $value;
-		}
-	}
+  /**
+   * Sets the value of the cookie object.
+   *
+   * @param string value The value to set the cookie to.
+   * @return bool TRUE on success.
+   */
+  function set($value)
+  {
+    if ( parent::set($value) )
+    {
+      if ($this->expire == 0)
+        $expire = 0;
+      else
+        $expire = time() + $this->expire;
+      
+      return setcookie($this->name, $this->value, $expire, $this->path, $this->domain, $this->secure);
+    }
 
-	/**
-	 * Sets the value of the cookie object.
-	 *
-	 * @param string value The value to set the cookie to.
-	 * @return bool TRUE on success.
-	 */
-	function set($value){
-		if ($this->regex == NULL || preg_match($this->regex, $value)) {
-			if (get_magic_quotes_gpc()) $value = stripslashes($value);
-			$this->value = $value;
-			if ($this->expire == 0) {
-				$expire = 0;
-			} else {
-				$expire = time() + $this->expire;
-			}
-			return setcookie($this->name, $this->value, $expire, $this->path, $this->domain, $this->secure);
-		} else {
-			return FALSE;
-		}
-	}
+    return FALSE;
+  }
 
-	/**
-	 * Delete the cookie from the client.
-	 *
-	 * @return bool TRUE on success.
-	 */
-	function delete(){
-		return setcookie($this->name, $this->value, time() - 3600, $this->path, $this->domain, $this->secure);
-	}
+  /**
+   * Delete the cookie from the client.
+   *
+   * @return bool TRUE on success.
+   */
+  function delete()
+  {
+    return setcookie($this->name, 
+                     $this->value, 
+                     time() - 3600, 
+                     $this->path, 
+                     $this->domain, 
+                     $this->secure);
+  }
 
 }
 
