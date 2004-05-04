68c9189c43081ca18d376b2ec084ad7c86d80a5f
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-05-04 10:21:15 +0000

    * Fixed errors when lines contain CRLF as lineending instead of the required   LF for sendmail. (Error occured when base64 encoded attachments with \r\n   as line termination were involved, That caused mixed usage of LF and CRLF   with as result a nice reject of the message by the sendmail binary from   postfix. * Documentation

--- squirrelmail/class/deliver/Deliver_SendMail.class.php
+++ squirrelmail/class/deliver/Deliver_SendMail.class.php
@@ -2,24 +2,55 @@
 /**
  * Deliver_SendMail.class.php
  *
- * Copyright (c) 1999-2004 The SquirrelMail Project Team
+ * @copyright Copyright (c) 1999-2004 The SquirrelMail Project Team
  * Licensed under the GNU GPL. For full terms see the file COPYING.
  *
  * Delivery backend for the Deliver class.
  *
  * $Id$
+ *
+ * @author Marc Groot Koerkamp
+ *
+ * @package squirrelmail
  */
 
+
+/** This of course depends upon Deliver */
 require_once(SM_PATH . 'class/deliver/Deliver.class.php');
+ 
+/**
+ * Delivers messages using the sendmail binary 
+ */
 
 class Deliver_SendMail extends Deliver {
 
+   /**
+    * function preWriteToStream
+    *
+    * Sendmail needs LF's as line endings instead of CRLF.
+    * This function translates the line endings to LF and should be called
+    * before each line is written to the stream. 
+    *  
+    * @param string $s Line to process
+    * @return void
+    * @access private
+    */ 
     function preWriteToStream(&$s) {
        if ($s) {
     	  $s = str_replace("\r\n", "\n", $s);
        }
     }
-    
+
+   /**
+    * function initStream
+    *
+    * Initialise the sendmail connection.
+    *  
+    * @param Message $message Message object containing the from address
+    * @param string $sendmail_path Location of sendmail binary
+    * @return void
+    * @access public 
+    */    
     function initStream($message, $sendmail_path) {
         $rfc822_header = $message->rfc822_header;
 	$from = $rfc822_header->from[0];
@@ -32,15 +63,53 @@ class Deliver_SendMail extends Deliver {
 	}
 	return $stream;
     }
-    
+
+   /**
+    * function finalizeStream
+    *
+    * Close the stream.
+    *  
+    * @param resource $stream
+    * @return boolean
+    * @access public
+    */    
     function finalizeStream($stream) {
 	pclose($stream);
 	return true;
     }
-    
+
+   /**
+    * function getBcc
+    *
+    * In case of sendmail, the rfc822header must contain the bcc header.
+    *  
+    * @return boolean true if rfc822header should include the bcc header.
+    * @access private
+    */
     function getBcc() {
        return true;
     }
     
+   /**
+    * function clean_crlf
+    *
+    * Cleans each line to only end in a LF
+    * Returns the length of the line including a CR, 
+    * so that length is correct when the message is saved to imap
+    * Implemented to fix sendmail->postfix rejection of messages with 
+    * attachments because of stray LF's
+    *
+    * @param string $s string to strip of CR's 
+    * @return integer length of string including a CR for each LF
+    * @access private 
+    */
+    function clean_crlf(&$s) {
+        $s = str_replace("\r\n", "\n", $s);
+        $s = str_replace("\r", "\n", $s);
+        $s2 = str_replace("\n", "\r\n", $s);
+        return strlen($s2);
+    }
+    
+
 }
 ?>
