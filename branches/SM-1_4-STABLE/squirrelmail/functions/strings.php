be1731c34a19867bf2c35f16bb98239aca5bf726
jangliss <jangliss@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-05-11 01:50:32 +0000

    Getting ready for a release candidate

--- branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
@@ -16,7 +16,7 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.3 [CVS]';
+$version = '1.4.3-RC1';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
