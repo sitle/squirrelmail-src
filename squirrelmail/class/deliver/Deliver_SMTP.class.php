458f72228ea61d355c4112ac4f1769ac80ef6a3d
stekkel <stekkel@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-05-21 18:02:31 +0000

    Fixed typo. Thnx to paul_s - psunners for spotting this.

--- squirrelmail/class/deliver/Deliver_SMTP.class.php
+++ squirrelmail/class/deliver/Deliver_SMTP.class.php
@@ -88,7 +88,7 @@ class Deliver_SMTP extends Deliver {
         $tmp = fgets($stream,1024);
         if ($this->errorCheck($tmp,$stream)) {
             // fall back to HELO if EHLO is n ot supported
-            if ($this->dlv_ret_no == '500') {
+            if ($this->dlv_ret_nr == '500') {
                 fputs($stream, "HELO $helohost\r\n");
                 $tmp = fgets($stream,1024);
                 if ($this->errorCheck($tmp,$stream)) {
