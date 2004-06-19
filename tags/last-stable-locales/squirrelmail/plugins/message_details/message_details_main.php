f21eb6da90a06c6fb710ff1f25f4085ac5906bcb
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-06-19 11:19:49 +0000

    This commit was manufactured by cvs2svn to create tag 'last-stable-locales'.

---
+++ tags/last-stable-locales/squirrelmail/plugins/message_details/message_details_main.php
@@ -0,0 +1,29 @@
+<?php
+/** Message Source  
+ *
+ * Plugin to view the RFC822 raw message output and the bodystructure of a message
+ *
+ * Copyright (c) 2002 Marc Groot Koerkamp, The Netherlands
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ * 
+ * $Id$
+ */
+
+/* Path for SquirrelMail required files. */
+define('SM_PATH','../../');
+
+/* SquirrelMail required files. */
+require_once(SM_PATH . 'include/validate.php');
+require_once(SM_PATH . 'functions/page_header.php');
+require_once(SM_PATH . 'functions/strings.php');
+
+displayHtmlHeader( _("Message Details"), '', FALSE );
+
+sqgetGlobalVar('mailbox', $mailbox, SQ_GET);
+sqgetGlobalVar('passed_id', $passed_id, SQ_GET);
+
+echo "<frameset rows=\"60, *\" noresize border=\"0\">\n";
+echo '<frame src="message_details_top.php?mailbox=' . urlencode($mailbox) .'&passed_id=' . "$passed_id". '" name="top_frame" scrolling="off">';
+echo '<frame src="message_details_bottom.php?mailbox=' . urlencode($mailbox) .'&passed_id=' . "$passed_id" . '" name="bottom_frame">';
+echo  '</frameset>'."\n"."</html>\n";
+?>
