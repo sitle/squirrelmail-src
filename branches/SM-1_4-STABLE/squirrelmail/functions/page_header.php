0f3ccc31a16164618a7444abdd1425f024f31894
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-07-30 12:59:48 +0000

    Added 'no focus' option for compose screen

--- branches/SM-1_4-STABLE/squirrelmail/functions/page_header.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/page_header.php
@@ -159,6 +159,12 @@ function displayPageHeader($color, $mailbox, $xtra='', $session=false) {
             {
                 if ($reply_focus == 'select') $js .= "document.forms['compose'].body.select();}\n";
                 else if ($reply_focus == 'focus') $js .= "document.forms['compose'].body.focus();}\n";
+                else if ($reply_focus == 'none') $js .= "}\n";
+            }
+            // no reply focus also applies to composing new messages
+            else if ($reply_focus == 'none')
+            {
+                $js .= "}\n";
             }
             else
                 $js .= "var f = document.forms.length;\n".
