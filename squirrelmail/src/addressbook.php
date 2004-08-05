41375802511a54a30ccab9eddab283020c3958f7
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-08-05 08:19:14 +0000

    Uneditable address book entries no longer have checkboxes next to them on addresses page (Thanks to Rolan Yang for patch)

--- squirrelmail/src/addressbook.php
+++ squirrelmail/src/addressbook.php
@@ -353,23 +353,36 @@ if ($showaddrlist) {
             }
             if ($squirrelmail_language == 'ja_JP')
                 {
-            echo html_tag( 'tr', '', '', $tr_bgcolor) .
+            echo html_tag( 'tr', '', '', $tr_bgcolor);
+            if ($abook->backends[$row['backend']]->writeable) {
                 html_tag( 'td',
                           '<small>' .
 			  addCheckBox('sel[]', $selected, $row['backend'].':'.$row['nickname']).
                           '</small>' ,
-                          'center', '', 'valign="top" width="1%"' ) .
-                html_tag( 'td', '&nbsp;' . $row['nickname'] . '&nbsp;', 'left', '', 'valign="top" width="1%" nowrap' ) . 
+                          'center', '', 'valign="top" width="1%"' );
+            } else {
+                echo html_tag( 'td',
+                        '&nbsp;' ,
+                        'center', '', 'valign="top" width="1%"' );
+            }
+            echo html_tag( 'td', '&nbsp;' . $row['nickname'] . '&nbsp;', 'left', '', 'valign="top" width="1%" nowrap' ) . 
                 html_tag( 'td', '&nbsp;' . $row['lastname'] . ' ' . $row['firstname'] . '&nbsp;', 'left', '', 'valign="top" width="1%" nowrap' ) .
                 html_tag( 'td', '', 'left', '', 'valign="top" width="1%" nowrap' ) . '&nbsp;';
                 } else {
-            echo html_tag( 'tr', '', '', $tr_bgcolor) .
-            html_tag( 'td',
-                '<small>' .
-		addCheckBox('sel[]', $selected, $row['backend'] . ':' . $row['nickname']).
-                '</small>' ,
-                'center', '', 'valign="top" width="1%"' ) .
-            html_tag( 'td', '&nbsp;' . $row['nickname'] . '&nbsp;', 'left', '', 'valign="top" width="1%" nowrap' ) .
+            echo html_tag( 'tr', '', '', $tr_bgcolor);
+            if ($abook->backends[$row['backend']]->writeable) {
+                echo html_tag( 'td',
+                       '<small>' .
+                       addCheckBox('sel[]', $selected, $row['backend'] . ':' . $row['nickname']).
+                       '</small>' ,
+                       'center', '', 'valign="top" width="1%"' );
+            } else {
+                echo html_tag( 'td',
+                        '&nbsp;' ,
+                        'center', '', 'valign="top" width="1%"' );
+            }
+
+            echo html_tag( 'td', '&nbsp;' . $row['nickname'] . '&nbsp;', 'left', '', 'valign="top" width="1%" nowrap' ) .
             html_tag( 'td', '&nbsp;' . $row['name'] . '&nbsp;', 'left', '', 'valign="top" width="1%" nowrap' ) .
             html_tag( 'td', '', 'left', '', 'valign="top" width="1%" nowrap' ) . '&nbsp;';
                 }
