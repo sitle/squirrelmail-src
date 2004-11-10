bf87fc1b241a554f11ad1dd413cf29f54d45073f
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2004-11-10 17:18:43 +0000

    fixed random theme

--- branches/SM-1_4-STABLE/squirrelmail/themes/random.php
+++ branches/SM-1_4-STABLE/squirrelmail/themes/random.php
@@ -1,24 +1,26 @@
 <?php
 
 /**
- * random.php
- *    Name:    Random Theme Every Login
- *    Author:  Tyler Akins
- *    Date:    December 24, 2001
- *    Comment: Guess what this does!
+ * Name:    Random Theme Every Login
+ * @author  Tyler Akins
+ * Date:    December 24, 2001
+ * Comment: Guess what this does!
  *
  * Copyright (c) 2000-2004 The SquirrelMail Project Team
  * Licensed under the GNU GPL. For full terms see the file COPYING.
  *
  * $Id$
+ * @package squirrelmail
+ * @subpackage themes
  */
 
+/** Initialize the random number generator */
 sq_mt_randomize();
 
 require_once(SM_PATH . 'functions/global.php');
-   
+
 global $theme;
-   
+
 if (!sqsession_is_registered('random_theme_good_theme')) {
     $good_themes = array();
     foreach ($theme as $data) {
@@ -31,12 +33,16 @@ if (!sqsession_is_registered('random_theme_good_theme')) {
     }
     $which = mt_rand(0, count($good_themes));
     $random_theme_good_theme = $good_themes[$which];
+    // remove current sm_path from theme name
+    $path=preg_quote(SM_PATH,'/');
+    $random_theme_good_theme=preg_replace("/^$path/",'',$random_theme_good_theme);
+    // store it in session
     sqsession_register($random_theme_good_theme, 'random_theme_good_theme');
 } else {
     // get random theme stored in session
     sqgetGlobalVar('random_theme_good_theme',$random_theme_good_theme);
 }
-   
-@include_once ($random_theme_good_theme);
+
+@include_once (SM_PATH . $random_theme_good_theme);
 
 ?>
