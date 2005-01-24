7567d9ac4bcb89c9cb89148d24baeb396775afb5
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-01-24 10:55:31 +0000

    Add support for Mail-Followup-To. In short, Reply To All will set To to the value of this header if set. http://cr.yp.to/proto/replyto.html http://www.ietf.org/proceedings/98dec/I-D/draft-ietf-drums-mail-followup-to-00.txt
    Backport from devel where it has been for 1.5 years.

--- squirrelmail/src/compose.php
+++ squirrelmail/src/compose.php
@@ -749,16 +749,22 @@ function newMail ($mailbox='', $passed_id='', $passed_ent_id='', $action='', $se
                 $body = '';
                 break;
             case ('reply_all'):
-                $send_to_cc = replyAllString($orig_header);
-                $send_to_cc = decodeHeader($send_to_cc,false,false,true);
-            case ('reply'):
-                $send_to = $orig_header->reply_to;
-                if (is_array($send_to) && count($send_to)) {
-                    $send_to = $orig_header->getAddr_s('reply_to');
-                } else if (is_object($send_to)) { /* unnessecarry, just for falesafe purpose */
-                    $send_to = $orig_header->getAddr_s('reply_to');
+                if(isset($orig_header->mail_followup_to) && $orig_header->mail_followup_to) {
+                    $send_to = $orig_header->getAddr_s('mail_followup_to');
                 } else {
-                    $send_to = $orig_header->getAddr_s('from');
+                    $send_to_cc = replyAllString($orig_header);
+                    $send_to_cc = decodeHeader($send_to_cc,false,false,true);
+                }
+            case ('reply'):
+                if (!$send_to) {
+                    $send_to = $orig_header->reply_to;
+                    if (is_array($send_to) && count($send_to)) {
+                        $send_to = $orig_header->getAddr_s('reply_to');
+                    } else if (is_object($send_to)) { /* unneccesarry, just for failsafe purpose */
+                        $send_to = $orig_header->getAddr_s('reply_to');
+                    } else {
+                        $send_to = $orig_header->getAddr_s('from');
+                    }
                 }
                 $send_to = decodeHeader($send_to,false,false,true);
                 $subject = decodeHeader($orig_header->subject,false,false,true);
