8468a6e80403fa2462b6cb9aea66b32c04a9fb7f
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-02-04 10:11:23 +0000

    added missing backend check.

--- branches/SM-1_4-STABLE/squirrelmail/src/addressbook.php
+++ branches/SM-1_4-STABLE/squirrelmail/src/addressbook.php
@@ -170,7 +170,11 @@ if(sqgetGlobalVar('REQUEST_METHOD', $req_method, SQ_SERVER) && $req_method == 'P
      * Add new address                                *
      **************************************************/
     if (isset($addaddr)) {
-        $r = $abook->add($addaddr, $abook->localbackend);
+        if (isset($backend)) {
+            $r = $abook->add($addaddr, $backend);
+        } else {
+            $r = $abook->add($addaddr, $abook->localbackend);
+        }
 
         /* Handle error messages */
         if (!$r) {
