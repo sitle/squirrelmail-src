45d98ca05192e743acfc627ee315525302ecb9b0
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-02-08 11:40:03 +0000

    adding site files

---
+++ locales/support/smstats/site/includes/header.php
@@ -0,0 +1,11 @@
+<?php
+header( 'Content-Type: text/html; charset=iso-8859-1');
+echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "<meta http-equiv=\"Content-Language\" content=\"en\">\n";
+echo "<title>SquirrelMail i18n statistics</title>\n";
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/includes/squirrelmail.css\">\n";
+echo "</head>\n";
+echo "<body>\n";
+?>
---
+++ locales/support/smstats/site/www/help.php
@@ -0,0 +1,23 @@
+<?php
+require_once('header.php');
+
+echo "<h2>Help information about SquirrelMail statistics</h2>\n";
+
+echo "<p>under construction</p>";
+
+echo "<p>contact tokul at users.sourceforge.net</p>";
+?>
+
+<table cellpadding="2" cellspacing="0" border="0" width="100%" bgcolor="#ececec">
+<tr align="center"><td><font size="2">
+
+<a href="./index.php">index</a> |
+<a href="./about.php">about</a> |
+help
+
+</font></td></tr></table>
+</td></tr></table>
+
+<?php
+require_once('footer.php');
+?>
