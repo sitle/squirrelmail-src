2ddfeef7d65eede648460bed6f94fa2edc24be36
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-02-11 15:24:27 +0000

    Give an error to the user when SquirrelMail is not configured yet

--- squirrelmail/index.php
+++ squirrelmail/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * index.php -- Displays the main frameset
+ * index.php -- Redirect to the login page.
  *
  * Copyright (c) 1999-2005 The SquirrelMail Project Team
  * Licensed under the GNU GPL. For full terms see the file COPYING.
@@ -10,7 +10,15 @@
  *
  * $Id$
  */
+ 
+// are we configured yet?
+if( ! file_exists ( 'config/config.php' ) ) {
+    echo "<html><body><p><strong>ERROR:</strong> Config file \"<tt>config/config.php</tt>\" not found. " .
+        "You need to configure SquirrelMail before you can use it.</p></body></html>";
+    exit;
+}
 
+// if we are, go ahead to the login page.
 header('Location: src/login.php');
 
 ?>
