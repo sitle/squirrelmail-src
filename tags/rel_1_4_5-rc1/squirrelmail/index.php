1edc6d14b5aacfc8e3fda42bbbf84c969d78304a
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-06-16 06:03:10 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_4_5-rc1'.

---
+++ tags/rel_1_4_5-rc1/squirrelmail/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * index.php -- Redirect to the login page.
+ *
+ * Copyright (c) 1999-2005 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * Redirects to the login page.
+ *
+ * $Id$
+ */
+ 
+// are we configured yet?
+if( ! file_exists ( 'config/config.php' ) ) {
+    echo "<html><body><p><strong>ERROR:</strong> Config file \"<tt>config/config.php</tt>\" not found. " .
+        "You need to configure SquirrelMail before you can use it.</p></body></html>";
+    exit;
+}
+
+// if we are, go ahead to the login page.
+header('Location: src/login.php');
+
+?>
+<html></html>
