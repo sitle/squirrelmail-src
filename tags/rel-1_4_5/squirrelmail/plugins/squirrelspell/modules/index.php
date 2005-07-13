f067f7a5a6c12c4147fe05607f4089387d934512
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-07-13 18:37:52 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1_4_5'.

---
+++ tags/rel-1_4_5/squirrelmail/plugins/squirrelspell/modules/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * index.php -- Displays the main frameset
+ *
+ * Copyright (c) 1999-2005 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * Redirects to the initial SquirrelMail page.
+ *
+ * @version $Id$
+ * @package plugins
+ * @subpackage squirrelspell
+ */
+header("Location:../../../index.php\n\n");
+exit();
+?>
