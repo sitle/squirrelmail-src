9b9e6c2c9464a4ac6c886e1482b36ad6779cb66d
tokul <tokul@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2005-11-06 09:41:05 +0000

    don't list index.php in sound selection box

--- squirrelmail/plugins/newmail/newmail_opt.php
+++ squirrelmail/plugins/newmail/newmail_opt.php
@@ -136,7 +136,7 @@ if ($allowsound == "true") {
     $d = dir(SM_PATH . 'plugins/newmail/sounds');
     while($entry=$d->read()) {
         $fname = get_location () . '/sounds/' . $entry;
-        if ($entry != '..' && $entry != '.' && $entry != 'CVS') {
+        if ($entry != '..' && $entry != '.' && $entry != 'CVS' && $entry != 'index.php') {
             echo '<option ';
             if ($fname == $media) {
                 echo 'selected="selected" ';
