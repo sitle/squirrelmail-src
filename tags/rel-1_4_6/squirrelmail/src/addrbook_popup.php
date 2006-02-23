6164e33bca9863578ce15297a4ccaf5f31e9351e
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-02-23 21:17:44 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1_4_6'.

---
+++ tags/rel-1_4_6/squirrelmail/src/addrbook_popup.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * addrbook_popup.php
+ *
+ * Copyright (c) 1999-2006 The SquirrelMail Project Team
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * Frameset for the JavaScript version of the address book.
+ *
+ * @version $Id$
+ * @package squirrelmail
+ */
+
+/**
+ * Path for SquirrelMail required files.
+ * @ignore
+ */
+define('SM_PATH','../');
+
+/** SquirrelMail required files. */
+require_once(SM_PATH . 'include/validate.php');
+require_once(SM_PATH . 'functions/addressbook.php');
+   
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+
+<html>
+    <head>
+        <meta name="robots" content="noindex,nofollow">
+        <title><?php echo "$org_title: " . _("Address Book"); ?></title>
+    </head>
+    <frameset rows="60,*" border="0">
+        <frame name="abookmain"
+               marginwidth="0"
+               scrolling="no"
+               border="0"
+               src="addrbook_search.php?show=form" />
+        <frame name="abookres"
+               marginwidth="0"
+               border="0"
+               src="addrbook_search.php?show=blank" />
+    </frameset>
+</html>
