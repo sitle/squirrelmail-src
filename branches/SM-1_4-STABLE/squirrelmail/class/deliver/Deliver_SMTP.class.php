12af9675bd0e87e66d461fab9b56b9845386d5bc
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-05-12 14:54:09 +0000

    - Improve recovery when EHLO not supported on legacy SMTP servers   (#1031455).

--- branches/SM-1_4-STABLE/squirrelmail/class/deliver/Deliver_SMTP.class.php
+++ branches/SM-1_4-STABLE/squirrelmail/class/deliver/Deliver_SMTP.class.php
@@ -93,8 +93,8 @@ class Deliver_SMTP extends Deliver {
         fputs($stream, "EHLO $helohost\r\n");
         $tmp = fgets($stream,1024);
         if ($this->errorCheck($tmp,$stream)) {
-            // fall back to HELO if EHLO is not supported
-            if ($this->dlv_ret_nr == '500') {
+            // fall back to HELO if EHLO is not supported (error 5xx)
+            if ($this->dlv_ret_nr{0} == '5') {
                 fputs($stream, "HELO $helohost\r\n");
                 $tmp = fgets($stream,1024);
                 if ($this->errorCheck($tmp,$stream)) {
