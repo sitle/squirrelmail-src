fc57ca951a046b1d72f83cc9bd11291c67e8f760
nobody <nobody@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-07-02 17:52:57 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1_4_7-20060702'.

---
+++ tags/rel-1_4_7-20060702/locales/support/smstats/cleanup-config.php
@@ -0,0 +1,24 @@
+<?php
+
+////////////////////////////////////////////////////////////////////////////
+// $Id$
+//
+// Description: configuration file for statistics cleanup
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+// development options
+$debug=100;
+
+
+$prog="cleanup-guistats";
+$adminemail="tomas@topolis.lt";
+
+// MySQL connection data
+$sql_user   = "smstats";
+$sql_pass   = "";
+$sql_host   = "localhost";
+$sql_db     = "smstats";
+
+?>
