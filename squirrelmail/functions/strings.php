e25d419e523c274d553b4379668aa50017e71334
jangliss <jangliss@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-07-04 18:48:56 +0000

    Update version string

--- squirrelmail/functions/strings.php
+++ squirrelmail/functions/strings.php
@@ -16,7 +16,7 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.7 [CVS]';
+$version = '1.4.7';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
