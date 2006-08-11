cdac2d66b8d547d05d508da143c2f332e56c1c32
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2006-08-11 11:24:05 +0000

    cvs now defrosted and ready for continued development

--- squirrelmail/functions/strings.php
+++ squirrelmail/functions/strings.php
@@ -16,14 +16,14 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.8';
+$version = '1.4.9 [CVS]';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
  * $sm_internal_version = array (release, major, minor)
  */
 global $SQM_INTERNAL_VERSION;
-$SQM_INTERNAL_VERSION = array(1,4,8);
+$SQM_INTERNAL_VERSION = array(1,4,9);
 
 /**
  * There can be a circular issue with includes, where the $version string is
