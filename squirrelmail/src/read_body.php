28b5f62eeca56642e45aeb3d6224f44ca162322f
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-06-25 15:21:29 +0000

    fix switch of en/decode in mdn, thanks Masato HIGASHIYAMA (#1694687)

--- squirrelmail/src/read_body.php
+++ squirrelmail/src/read_body.php
@@ -176,7 +176,7 @@ function SendMDN ( $mailbox, $passed_id, $sender, $message, $imapConnection) {
     }
     $rfc822_header->content_type = $content_type;
     $rfc822_header->to[] = $header->dnt;
-    $rfc822_header->subject = _("Read:") . ' ' . encodeHeader($header->subject);
+    $rfc822_header->subject = _("Read:") . ' ' . decodeHeader($header->subject, true, false);
 
     // FIX ME, use identity.php from SM 1.5. Change this also in compose.php
 
