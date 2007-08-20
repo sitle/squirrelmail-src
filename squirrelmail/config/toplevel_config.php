0fd968e19cdd16f274ec4b9003d79126e2635e2c
bouchon <bouchon@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-08-20 14:02:19 +0000

    Added command line tool (works for top-level config only for now)

--- squirrelmail/config/toplevel_config.php
+++
@@ -1,7 +0,0 @@
-; This is the top-level configuration file of SquirrelMail. <?php die(); ?>
-
-admin_password = "ef927b98dc7161af777283b0f43b1e44"
-squirrelmail_default_language = "en_US"
-default_charset = "iso-8859-1"
-config_backend = "file"
-config_file[] = "meta_config.php,default_config.php,config.php"
