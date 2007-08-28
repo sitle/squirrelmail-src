82dea5f3e05582118f5d7d4c55a49ad691d511eb
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-08-28 20:40:10 +0000

    Fix single-page session restores; change restore mechanism to use PAGE_NAME

--- branches/SM-1_4-STABLE/squirrelmail/functions/auth.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/auth.php
@@ -41,7 +41,7 @@ function is_logged_in() {
     if ( sqsession_is_registered('user_is_logged_in') ) {
         return;
     } else {
-        global $PHP_SELF, $session_expired_post,
+        global $session_expired_post,
                $session_expired_location, $squirrelmail_language;
 
         // use $message to indicate what logout text the user
@@ -54,13 +54,13 @@ function is_logged_in() {
         //  First we store some information in the new session to prevent
         //  information-loss.
         $session_expired_post = $_POST;
-        $session_expired_location = $PHP_SELF;
+        $session_expired_location = PAGE_NAME;
         if (!sqsession_is_registered('session_expired_post')) {
             sqsession_register($session_expired_post,'session_expired_post');
         }
         if (!sqsession_is_registered('session_expired_location')) {
             sqsession_register($session_expired_location,'session_expired_location');
-            if (stristr($session_expired_location, 'src/compose.php'))
+            if ($session_expired_location == 'compose')
                 $message = 1;
         }
 
