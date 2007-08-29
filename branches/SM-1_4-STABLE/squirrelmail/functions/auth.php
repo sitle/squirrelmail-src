92314c38d7bb9fd86fb5e0bb0bde973d0e99d74a
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-08-29 23:59:12 +0000

    Added smtp_auth hook

--- branches/SM-1_4-STABLE/squirrelmail/functions/auth.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/auth.php
@@ -273,5 +273,16 @@ function get_smtp_user(&$user, &$pass) {
         $user = $username;
         $pass = sqauth_read_password();
     }
+
+    // plugin authors note: override $user or $pass by
+    // returning an array where the new username is the
+    // first array value and the new password is the 
+    // second array value e.g., return array($myuser, $mypass);
+    //
+    $ret = do_hook_function('smtp_auth', array($user, $pass));
+    if (!empty($ret[0]))
+        $user = $ret[0];
+    if (!empty($ret[1]))
+        $pass = $ret[1];
 }
 
