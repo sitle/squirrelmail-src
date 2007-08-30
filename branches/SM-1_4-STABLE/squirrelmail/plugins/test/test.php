c2e619f1adef302254b10b3b52205c43c7cb12e3
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-08-30 03:40:13 +0000

    Initial import from trunk/plugins

---
+++ branches/SM-1_4-STABLE/squirrelmail/plugins/test/test.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * SquirrelMail Test Plugin
+ * @copyright &copy; 2006 The SquirrelMail Project Team
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id: test.php 11089 2006-04-24 20:08:48Z tokul $
+ * @package plugins
+ * @subpackage test
+ */
+
+
+define('SM_PATH', '../../');
+include_once(SM_PATH . 'include/validate.php');
+
+global $color;
+displayPageHeader($color, 'none');
+
+?>
+
+<strong>Tests:</strong>
+<br />
+<br />
+<p><a href="decodeheader.php">decodeHeader() test</a></p>
+<p><a href="ngettext.php">ngettext() test</a></p>
+
+</body>
+</html>
+
