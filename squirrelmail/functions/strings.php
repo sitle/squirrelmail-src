5143f7f029d234c456c77f4d795d67d2bf7ffc50
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-09-29 07:54:57 +0000

    prepare for release of 1.4.11

--- squirrelmail/functions/strings.php
+++ squirrelmail/functions/strings.php
@@ -16,7 +16,7 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.11 [SVN]';
+$version = '1.4.11';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
