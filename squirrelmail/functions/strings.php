d3d518b2a3490ef2d649b7fbaa8d986a89e82daf
jangliss <jangliss@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-12-05 05:24:24 +0000

    Preparing for the 1.4.12 release

--- squirrelmail/functions/strings.php
+++ squirrelmail/functions/strings.php
@@ -16,7 +16,7 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.12 [SVN]';
+$version = '1.4.12';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
