45ef419a1e3a74a0428454b88cf544012a597b5f
jangliss <jangliss@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2007-12-14 17:04:21 +0000

    Gearing up for 1.4.13 release

--- branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
@@ -16,7 +16,7 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.13 [SVN]';
+$version = '1.4.13';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
