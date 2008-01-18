3d267640481caf384ee23ab48f8d890af6630d85
jervfors <jervfors@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2008-01-18 12:25:38 +0000

    Changing the language name "Farsi" to "Persian" per request from Abbas Esmaeeli Some'eh <someeh at gmail.com> (approved by Reza Mohammadi).

--- trunk/locales/locale/fa_IR/setup.php
+++ trunk/locales/locale/fa_IR/setup.php
@@ -2,7 +2,7 @@
 /**
  * Language configuration file
  *
- * Copyright (c) 2005-2006 The SquirrelMail Project Team
+ * Copyright (c) 2005-2008 The SquirrelMail Project Team
  *
  * This file is part of SquirrelMail webmail interface. It is distributed
  * together with other translation files and is used to enable 
@@ -26,7 +26,7 @@
  * @package locales
  */
 
-$languages['fa_IR']['NAME']    = 'Farsi';
+$languages['fa_IR']['NAME']    = 'Persian';
 $languages['fa_IR']['CHARSET'] = 'utf-8';
 $languages['fa_IR']['DIR']     = 'rtl';
 $languages['fa_IR']['LOCALE']  = array('fa_IR.UTF-8','fa_IR');
