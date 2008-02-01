144565b9b3dc82c21623f901245c85f2af9d3f3f
jervfors <jervfors@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2008-02-01 17:12:59 +0000

    Changing to UTF-8 per the 2007-12-10 decision by the nb_NO language team.

--- locales/locale/nb_NO/setup.php
+++ locales/locale/nb_NO/setup.php
@@ -2,7 +2,7 @@
 /**
  * Language configuration file
  *
- * Copyright (c) 2005-2006 The SquirrelMail Project Team
+ * Copyright (c) 2005-2008 The SquirrelMail Project Team
  *
  * This file is part of SquirrelMail webmail interface. It is distributed
  * together with other translation files and is used to enable 
@@ -28,6 +28,6 @@
 
 $languages['nb_NO']['NAME']    = 'Norwegian (Bokm&aring;l)';
 $languages['nb_NO']['ALTNAME'] = 'Norsk (Bokm&aring;l)';
-$languages['nb_NO']['CHARSET'] = 'iso-8859-1';
-$languages['nb_NO']['LOCALE']  = array('nb_NO.ISO8859-1','nb_NO.ISO-8859-1','nb_NO');
+$languages['nb_NO']['CHARSET'] = 'utf-8';
+$languages['nb_NO']['LOCALE']  = array('nb_NO.UTF-8','nb_NO');
 $languages['nb']['ALIAS'] = 'nb_NO';
