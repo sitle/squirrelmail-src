2588b0609eadf0622c040f42f3b90c8a094f6ce9
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2008-03-04 08:44:34 +0000

    Add convenience function; keep track of multi-value widget types in one place

--- branches/SM-1_4-STABLE/squirrelmail/functions/options.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/options.php
@@ -114,6 +114,14 @@ class SquirrelOption {
         }
     }
 
+    /** Convenience function that identifies which types of
+        widgets are stored as (serialized) array values. */
+    function is_multiple_valued() {
+        return ($this->type == SMOPT_TYPE_FLDRLIST_MULTI
+             || $this->type == SMOPT_TYPE_STRLIST_MULTI
+             || $this->type == SMOPT_TYPE_EDIT_LIST);
+    }
+
     /* Set the value for this option. */
     function setValue($value) {
         $this->value = $value;
@@ -623,8 +631,7 @@ function save_option($option) {
     // Certain option types need to be serialized because
     // they are not scalar
     //
-    } else if ($option->type == SMOPT_TYPE_FLDRLIST_MULTI
-            || $option->type == SMOPT_TYPE_STRLIST_MULTI)
+    } else if ($option->is_multiple_valued())
         setPref($data_dir, $username, $option->name, serialize($option->new_value));
     else
         setPref($data_dir, $username, $option->name, $option->new_value);
