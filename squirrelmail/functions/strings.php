a43a005a22036ec24ab947ed17d9a1d51ceb9033
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2008-05-23 17:49:33 +0000

    release for further development

--- squirrelmail/functions/strings.php
+++ squirrelmail/functions/strings.php
@@ -16,14 +16,14 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.15';
+$version = '1.4.16 [SVN]';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
  * $sm_internal_version = array (release, major, minor)
  */
 global $SQM_INTERNAL_VERSION;
-$SQM_INTERNAL_VERSION = array(1,4,15);
+$SQM_INTERNAL_VERSION = array(1,4,16);
 
 /**
  * There can be a circular issue with includes, where the $version string is
