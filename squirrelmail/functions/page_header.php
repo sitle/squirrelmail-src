39622d3751ec21412cb7c0436e6f1043e4b97696
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2008-09-28 13:19:08 +0000

    Fix HTML validity issue with IE conditional construct (#1985916). This construct may only be used in HTML context, not CSS context. Thanks Haeber

--- squirrelmail/functions/page_header.php
+++ squirrelmail/functions/page_header.php
@@ -53,14 +53,14 @@ function displayHtmlHeader( $title = 'SquirrelMail', $xtra = '', $do_hook = TRUE
 
     /* work around IE6's scrollbar bug */
     echo <<<ECHO
-<style type="text/css">
 <!--[if IE 6]>
+<style type="text/css">
 /* avoid stupid IE6 bug with frames and scrollbars */
 body {
     width: expression(document.documentElement.clientWidth - 30);
 }
-<![endif]-->
 </style>
+<![endif]-->
 
 ECHO;
 
