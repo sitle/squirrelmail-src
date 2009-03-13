e75185d2865ab42b39051a2d6a451379268c1114
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-03-13 01:55:54 +0000

    Try again

--- squirrelmail/functions/i18n.php
+++ squirrelmail/functions/i18n.php
@@ -742,13 +742,13 @@ $languages['bg_BG']['CHARSET'] = 'windows-1251';
 $languages['bg_BG']['LOCALE']  = 'bg_BG.CP1251';
 $languages['bg']['ALIAS']      = 'bg_BG';
 
-$languages['bn_BD']['NAME']    = 'Bangladeshi Bengali';
+$languages['bn_BD']['NAME']    = 'Bengali (Bangladesh)';
 $languages['bn_BD']['ALTNAME'] = 'Bangla';
 $languages['bn_BD']['CHARSET'] = 'utf-8';
 $languages['bn_BD']['LOCALE']  = array('bn_BD.UTF-8', 'bn_BD', 'bn.UTF-8', 'bn');
 $languages['bn']['ALIAS'] = 'bn_BD';
 
-$languages['bn_IN']['NAME']    = 'Indian Bengali';
+$languages['bn_IN']['NAME']    = 'Bengali (India)';
 $languages['bn_IN']['CHARSET'] = 'utf-8';
 $languages['bn_IN']['LOCALE']  = 'bn_IN.UTF-8';
 
