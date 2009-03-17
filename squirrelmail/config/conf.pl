055df777887bbb25725d28c6064944c8dad16f0c
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-03-17 21:23:46 +0000

    Don't use white color in highlight escape code. Thanks to Tomas Kuliavas

--- squirrelmail/config/conf.pl
+++ squirrelmail/config/conf.pl
@@ -396,7 +396,7 @@ $list_supported_imap_servers =
 
 #####################################################################################
 if ( $config_use_color == 1 ) {
-    $WHT = "\x1B[37;1m";
+    $WHT = "\x1B[1m";
     $NRM = "\x1B[0m";
 } else {
     $WHT              = "";
@@ -681,7 +681,7 @@ while ( ( $command ne "q" ) && ( $command ne "Q" ) ) {
             $NRM              = "";
         } else {
             $config_use_color = 1;
-            $WHT              = "\x1B[37;1m";
+            $WHT              = "\x1B[1m";
             $NRM              = "\x1B[0m";
         }
     } elsif ( $command eq "d" && $menu == 0 ) {
