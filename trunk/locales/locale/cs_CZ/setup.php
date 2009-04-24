0b28b6c3c3c594d05e2ad92e4ac74d3a7f50baef
jervfors <jervfors@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-04-24 16:20:57 +0000

    Converting Czech (cs_CZ) to UTF-8 as requested at the mailing list.

--- trunk/locales/locale/cs_CZ/setup.php
+++ trunk/locales/locale/cs_CZ/setup.php
@@ -28,6 +28,6 @@
 
 $languages['cs_CZ']['NAME']    = 'Czech';
 $languages['cs_CZ']['ALTNAME'] = '&#268;e&scaron;tina';
-$languages['cs_CZ']['CHARSET'] = 'iso-8859-2';
-$languages['cs_CZ']['LOCALE']  = array('cs_CZ.ISO8859-2','cs_CZ.ISO-8859-2','cs_CZ');
+$languages['cs_CZ']['CHARSET'] = 'utf-8';
+$languages['cs_CZ']['LOCALE']  = array('cs_CZ.UTF-8','cs_CZ');
 $languages['cs']['ALIAS']      = 'cs_CZ';
