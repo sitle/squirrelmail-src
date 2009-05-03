1983ccfa3860dc2722ed9517a2f56054350c0c71
jervfors <jervfors@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-05-03 12:33:46 +0000

    Adding a comment with an explanation for translators.

--- branches/SM-1_4-STABLE/squirrelmail/src/options_highlight.php
+++ branches/SM-1_4-STABLE/squirrelmail/src/options_highlight.php
@@ -412,7 +412,7 @@ if ($action == 'edit' || $action == 'add') {
         addInput('newcolor_input',
             (($selected_input && isset($theid)) ? $message_highlight_list[$theid]['color'] : ''),
             '7');
-    // i18n: This is an example on how to write a color in RGB.
+    // i18n: This is an example on how to write a color in RGB, literally meaning "For example: 63aa7f".
     echo _("Ex: 63aa7f")."<br />\n";
     echo "      </td>\n";
     echo "   </tr>\n";
