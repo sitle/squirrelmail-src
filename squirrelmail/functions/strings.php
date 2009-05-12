d0c4a85628b63a8f86321838ac560951cd12a5da
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-05-12 00:04:23 +0000

    1.4.19 SVN open for business

--- squirrelmail/functions/strings.php
+++ squirrelmail/functions/strings.php
@@ -16,14 +16,14 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.18';
+$version = '1.4.19 [SVN]';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
  * $sm_internal_version = array (release, major, minor)
  */
 global $SQM_INTERNAL_VERSION;
-$SQM_INTERNAL_VERSION = array(1,4,18);
+$SQM_INTERNAL_VERSION = array(1,4,19);
 
 /**
  * There can be a circular issue with includes, where the $version string is
