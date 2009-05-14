63086c6a1d06d4878900fe98533a5d9312d7115b
jervfors <jervfors@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-05-14 06:27:08 +0000

    Porting changes from HEAD.

--- locales/locale/uk_UA/setup.php
+++ locales/locale/uk_UA/setup.php
@@ -2,7 +2,7 @@
 /**
  * Language configuration file
  *
- * Copyright (c) 2005-2006 The SquirrelMail Project Team
+ * Copyright (c) 2005-2009 The SquirrelMail Project Team
  *
  * This file is part of SquirrelMail webmail interface. It is distributed
  * together with other translation files and is used to enable 
@@ -28,5 +28,5 @@
 
 $languages['uk_UA']['NAME']    = 'Ukrainian';
 $languages['uk_UA']['CHARSET'] = 'utf-8';
-$languages['uk_UA']['LOCALE']  = array('uk_UA.UTF-8','uk_UA','uk');
+$languages['uk_UA']['LOCALE']  = array('uk_UA.UTF-8', 'uk_UA.UTF8', 'uk_UA', 'uk');
 $languages['uk']['ALIAS'] = 'uk_UA';
