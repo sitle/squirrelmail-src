9054ffd19581684b1816b2208ed65a9f066ee3e5
kink <kink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2009-05-21 17:32:27 +0000

    reopen svn for continued development

--- branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
@@ -16,14 +16,14 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.19';
+$version = '1.4.20 [SVN]';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
  * $sm_internal_version = array (release, major, minor)
  */
 global $SQM_INTERNAL_VERSION;
-$SQM_INTERNAL_VERSION = array(1,4,19);
+$SQM_INTERNAL_VERSION = array(1,4,20);
 
 /**
  * There can be a circular issue with includes, where the $version string is
