c076b22f709ad683fc77c533e58a916dec9ce739
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2010-02-16 20:14:18 +0000

    Sync pref name with DEVEL

--- branches/SM-1_4-STABLE/squirrelmail/include/load_prefs.php
+++ branches/SM-1_4-STABLE/squirrelmail/include/load_prefs.php
@@ -257,7 +257,7 @@ $compose_width = getPref($data_dir, $username, 'compose_width', 640);
 $sig_first = getPref($data_dir, $username, 'sig_first', 0);
 
 /* strip signature from replies setting */
-$trim_signature_on_reply = getPref($data_dir, $username, 'trim_signature_on_reply', 0);
+$strip_sigs = getPref($data_dir, $username, 'strip_sigs', 0);
 
 /* use the internal date of the message for sorting instead of the supplied header date */
 $internal_date_sort = getPref($data_dir, $username, 'internal_date_sort', SMPREF_ON);
