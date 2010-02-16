63ed46c429e0dc1baf9b522b78e6e1a3097eea41
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2010-02-16 20:20:57 +0000

    Sync string with DEVEL, even though that one is poorly thought out and capitalized... there's probably already translations for it in SVN

--- branches/SM-1_4-STABLE/squirrelmail/include/options/display.php
+++ branches/SM-1_4-STABLE/squirrelmail/include/options/display.php
@@ -332,7 +332,7 @@ function load_optpage_data_display() {
 
     $optvals[SMOPT_GRP_MESSAGE][] = array(
         'name'    => 'strip_sigs',
-        'caption' => _("Remove Original Signature when Replying"),
+        'caption' => _("Strip signature when replying"),
         'type'    => SMOPT_TYPE_BOOLEAN,
         'refresh' => SMOPT_REFRESH_NONE
     );
