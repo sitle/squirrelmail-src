cda4961352289fbd0f125cebeaab48553b14cf6a
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2011-01-06 02:44:03 +0000

    Happy New Year!

--- squirrelmail/functions/constants.php
+++ squirrelmail/functions/constants.php
@@ -7,7 +7,7 @@
  * This file is include by src/login.php, src/redirect.php and
  * src/load_prefs.php.
  *
- * @copyright 1999-2010 The SquirrelMail Project Team
+ * @copyright 1999-2011 The SquirrelMail Project Team
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version $Id$
  * @package squirrelmail
