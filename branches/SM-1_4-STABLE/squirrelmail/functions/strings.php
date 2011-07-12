4258713d5cd8203b7e221639e865cde719498f45
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2011-07-12 20:34:34 +0000

    Version 1.4.23 ready for continued development

--- branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
+++ branches/SM-1_4-STABLE/squirrelmail/functions/strings.php
@@ -16,14 +16,14 @@
  * SquirrelMail version number -- DO NOT CHANGE
  */
 global $version;
-$version = '1.4.22';
+$version = '1.4.23 [SVN]';
 
 /**
  * SquirrelMail internal version number -- DO NOT CHANGE
  * $sm_internal_version = array (release, major, minor)
  */
 global $SQM_INTERNAL_VERSION;
-$SQM_INTERNAL_VERSION = array(1,4,22);
+$SQM_INTERNAL_VERSION = array(1, 4, 23);
 
 /**
  * There can be a circular issue with includes, where the $version string is
