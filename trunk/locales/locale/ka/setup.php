a72620fa5ddbf63f10ec33424e925469ce25c3dd
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2015-01-03 03:48:43 +0000

    Happy 2015

--- trunk/locales/locale/ka/setup.php
+++ trunk/locales/locale/ka/setup.php
@@ -2,7 +2,7 @@
 /**
  * Language configuration file
  *
- * Copyright (c) 2006-2014 The SquirrelMail Project Team
+ * Copyright (c) 2006-2015 The SquirrelMail Project Team
  *
  * This file is part of SquirrelMail webmail interface. It is distributed
  * together with other translation files and is used to enable 
